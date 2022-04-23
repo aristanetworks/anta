ac695f445c0a2c80364d7165f19a9ff1770730f2
Khelil Sator <ksator@arista.com>
2022-04-23 09:16:50 +0200

    Update collect_sheduled_show_tech.py

--- collect_sheduled_show_tech.py
+++ collect_sheduled_show_tech.py
@@ -93,8 +93,8 @@ def main():
 
     number_of_unreachable_devices = len(unreachable)
     number_of_reachable_devices = len(devices)
-    pbar = tqdm(total = number_of_unreachable_devices + number_of_reachable_devices)
-    pbar.update(number_of_unreachable_devices, desc = 'Collecting files from devices ... please be patient ...')
+    pbar = tqdm(total = number_of_unreachable_devices + number_of_reachable_devicess, desc = 'Collecting files from devices ... please be patient ...')
+    pbar.update(number_of_unreachable_device)
 
     # Collect all the tech-support files stored on Arista switches flash and copy them locally
 
