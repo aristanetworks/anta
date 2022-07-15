0b1a0c7caaa81400a1886b211cb11696e680406d
khelil sator <ksator@arista.com>
2022-07-15 20:21:57 +0200

    fix pylint issue - adding inv module to setup package

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     name="anta",
     version=f"{anta.__version__}",
     python_requires=">=3.7",
-    packages=['anta'],
+    packages=['anta', 'anta.inventory'],
     scripts=[
         "scripts/check-devices.py",
         "scripts/collect-eos-commands.py",
