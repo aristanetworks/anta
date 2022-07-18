cbeb569e1086cc1cbb251a1350ee7ae9c9e4bdba
khelil sator <ksator@arista.com>
2022-07-18 12:59:14 +0200

    update cvp script to output inv file

--- scripts/create-devices-inventory-from-cvp.py
+++ scripts/create-devices-inventory-from-cvp.py
@@ -5,7 +5,7 @@ This script:
 - uses CVP REST API to generate a token
 - uses cvprac with this token to get the device inventory
 - creates a text file with the devices IP address under the container passed in argument
-    - if we dont provide the argument `c` it creates a text file with all the devices IP address.
+- if we dont provide the argument `c` it creates a text file with all the devices IP address from the CVP inventory.
 
 usage: ./create-devices-inventory-from-cvp.py --help
 requirement: pip install cvprac==1.2.0
@@ -81,23 +81,30 @@ def main():
 
     if args.container is None:
         # Get a list of all devices
-        # help(clnt.api.get_inventory)
         inventory = clnt.api.get_inventory()
-        # write all IP addresses from the CVP inventory in the all.txt file
-        out_file = f'{out_dir}/all.txt'
+        # write the IP addresses in a file
+        out_file = f'{out_dir}/all.yml'
         with open(out_file, 'w', encoding="utf8") as out_file:
+            out_file.write("anta_inventory:")
+            out_file.write('\n')
+            out_file.write("  hosts:")
+            out_file.write('\n')
             for dev in inventory:
-                out_file.write(dev['ipAddress'])
+                out_file.write("  - host: " + dev['ipAddress'])
                 out_file.write('\n')
     else:
-        # Get devices under a container and write the devices IP address in a file
-        # help(clnt.api.get_devices_in_container)
+        # Get devices under a container
         container = args.container
         container_inventory = clnt.api.get_devices_in_container(container)
-        out_file = f'{out_dir}/{container}.txt'
+        # write the devices IP address in a file
+        out_file = f'{out_dir}/{container}.yml'
         with open(out_file, 'w', encoding="utf8") as out_file:
+            out_file.write("anta_inventory:")
+            out_file.write('\n')
+            out_file.write("  hosts:")
+            out_file.write('\n')
             for dev in container_inventory:
-                out_file.write(dev['ipAddress'])
+                out_file.write("  - host: " + dev['ipAddress'])
                 out_file.write('\n')
 
 if __name__ == '__main__':
