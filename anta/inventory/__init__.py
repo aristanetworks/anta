c2ee24eb8fefa8979dcd63972c48b79f87a48806
Thomas Grimonet <thomas.grimonet@gmail.com>
2022-07-20 15:44:33 +0200

    fix(inventory): incorrect docstring syntax

--- anta/inventory/__init__.py
+++ anta/inventory/__init__.py
@@ -32,7 +32,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 class AntaInventory():
-        """
+    """
     Inventory Abstraction for ANTA framework.
 
     Attributes:
