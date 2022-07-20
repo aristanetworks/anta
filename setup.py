2df222a518579fe3d688295f841396fbdca3110b
gmuloc <gmulocher@arista.com>
2022-07-20 17:34:36 +0200

    Fix: spelling mistake for filename in setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         "scripts/collect-eos-commands.py",
         "scripts/check-devices-reachability.py",
         "scripts/clear-counters.py",
-        "scripts/collect-sheduled-show-tech.py",
+        "scripts/collect-scheduled-show-tech.py",
         "scripts/evpn-blacklist-recovery.py",
         "scripts/create-devices-inventory-from-cvp.py"
     ],
