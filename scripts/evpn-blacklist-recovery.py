b99847d8fb4e0339b6574ea47cea109d04fdbf24
Khelil Sator <ksator@arista.com>
2022-07-20 08:47:41 +0200

    feat(scripts): update evpn script with inventory module(#51)
    * update evpn-blacklist-recovery.py script
    
    * update evpn script to use inv module
    
    * fix evpn script string

--- scripts/evpn-blacklist-recovery.py
+++ scripts/evpn-blacklist-recovery.py
@@ -7,14 +7,40 @@ This script clears on devices the list of MAC addresses which are blacklisted in
 from argparse import ArgumentParser
 from getpass import getpass
 import ssl
-import sys
-from socket import setdefaulttimeout
-from jsonrpclib import Server,jsonrpc
+import logging
+from jsonrpclib import jsonrpc
+from anta.inventory import AntaInventory
 
 # pylint: disable=protected-access
 ssl._create_default_https_context = ssl._create_unverified_context
+logging.disable(level=logging.WARNING)
+
+def clear_evpn_blacklisted_mac_addresses(inventory, enable_pass):
+    """
+    clear EVPN blacklisted mac addresses
+    """
+    devices = inventory.get_inventory(established_only = True)
+    for device in devices:
+        switch = device.session
+        try:
+            switch.runCmds(1,[{"cmd": "enable", "input": enable_pass},\
+                 'clear bgp evpn host-flap'])
+        except jsonrpc.AppError:
+            print(f"Could not clear the EVPN blacklisted mac addresses on device {str(device.host)}")
+
+def report_unreachable_devices(inventory):
+    """
+    report unreachable devices
+    """
+    devices = inventory.get_inventory(established_only = False)
+    for device in devices:
+        if device.established is False:
+            print(f"Could not connect to device {str(device.host)}")
 
 def main():
+    """
+    clear EVPN blacklisted mac addresses
+    """
     parser = ArgumentParser(
         description='Clear the list of MAC addresses which are blacklisted in EVPN'
         )
@@ -34,32 +60,16 @@ def main():
     args.password = getpass(prompt='Device password: ')
     args.enable_pass = getpass(prompt='Enable password (if any): ')
 
-    try:
-        with open(args.file, 'r', encoding='utf8') as file:
-            devices = file.readlines()
-    except FileNotFoundError:
-        print('Error reading ' + args.file)
-        sys.exit(1)
-
-    for i,device in enumerate(devices):
-        devices[i] = device.strip()
-
-    print('Clearing on all the devices the list of MAC addresses which are blacklisted in EVPN ...')
-
-    for device in devices:
-        try:
-            setdefaulttimeout(5)
-            url=f"https://{args.username}:{args.password}@{device}/command-api"
-            switch = Server(url)
-            switch.runCmds(1,[{"cmd": "enable", "input": args.enable_pass},\
-                 'clear bgp evpn host-flap'])
-        except jsonrpc.TransportError:
-            print('wrong credentials for ' + device)
-        except OSError:
-            print(device + ' is not reachable using eAPI')
-        except jsonrpc.AppError:
-            print("Could not run this command on device " + device)
-    print('done')
+    print('Clearing on all the devices the list of MAC addresses which are blacklisted in EVPN ... please be patient ...')
+    inventory = AntaInventory(
+        inventory_file=args.file,
+        username=args.username,
+        password=args.password,
+        auto_connect=True,
+        timeout=1
+    )
+    clear_evpn_blacklisted_mac_addresses(inventory, args.enable_pass)
+    report_unreachable_devices(inventory)
 
 if __name__ == '__main__':
     main()
