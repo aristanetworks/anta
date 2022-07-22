ba4f7c19d2f9816923027602341206f1b8dfb7d1
Thomas Grimonet <tgrimonet@arista.com>
2022-07-22 18:33:00 +0200

    refactor(tests): Update SOFTWARE tests to support new structures (#75)

--- anta/tests/software.py
+++ anta/tests/software.py
@@ -1,62 +1,90 @@
 """
 Test functions related to the EOS software
 """
-from jsonrpclib import jsonrpc
+from typing import List
 
-def verify_eos_version(device, enable_password, versions = None):
+from jsonrpclib import jsonrpc
+from anta.inventory.models import InventoryDevice
+from anta.result_manager.models import TestResult
+
+
+def verify_eos_version(device: InventoryDevice, versions: List[str] = None) -> TestResult:
     """
     Verifies the device is running one of the allowed EOS version.
 
     Args:
-        device (jsonrpclib.jsonrpc.ServerProxy): Instance of the class jsonrpclib.jsonrpc.ServerProxy with the uri f'https://{username}:{password}@{ip}/command-api'.
-        enable_password (str): Enable password.
+        device (InventoryDevice): InventoryDevice instance containing all devices information.
         versions (list): List of allowed EOS versions.
 
     Returns:
-        bool: `True` if the device is running an allowed EOS version.
-        `False` otherwise.
+        TestResult instance with
+        * result = "unset" if test has not been executed
+        * result = "success" if EOS version is valid against versions
+        * result = "failure" otherwise.
+        * result = "error" if any exception is caught
 
     """
+    result = TestResult(host=str(device.host),
+                        test="verify_eos_version")
     if not versions:
-        return None
-    try:
-        response = device.runCmds(1, ['show version'], 'json')
-    except jsonrpc.AppError:
-        return None
-    try:
-        if response[0]['version'] in versions:
-            return True
-        return False
-    except KeyError:
-        return None
+        result.result = "unset"
+        result.messages.append(
+            "verify_eos_version was not run as no "
+            "versions were givem"
+        )
+        return result
 
-def verify_terminattr_version(device, enable_password, versions = None):
+    try:
+        response = device.session.runCmds(1, ['show version'], 'json')
+        if response[0]['version'] in versions:
+            result.result = 'success'
+        else:
+            result.result = 'failure'
+            result.messages.append(f'device is running version {response[0]["version"]} not in expected version')
+    except (jsonrpc.AppError, KeyError) as e:
+        result.messages.append(str(e))
+        result.result = 'error'
+    return result
+
+
+def verify_terminattr_version(device: InventoryDevice, versions=None) -> TestResult:
     """
     Verifies the device is running one of the allowed TerminAttr version.
 
     Args:
-        device (jsonrpclib.jsonrpc.ServerProxy): Instance of the class jsonrpclib.jsonrpc.ServerProxy with the uri f'https://{username}:{password}@{ip}/command-api'.
-        enable_password (str): Enable password.
+        device (InventoryDevice): InventoryDevice instance containing all devices information.
         versions (list): List of allowed TerminAttr versions.
 
     Returns:
-        bool: `True` if the device is running an allowed TerminAttr version. `False` otherwise.
-
+        TestResult instance with
+        * result = "unset" if test has not been executed
+        * result = "success" if TerminAttr version is valid against versions
+        * result = "failure" otherwise.
+        * result = "error" if any exception is caught
     """
+    result = TestResult(host=str(device.host),
+                        test="verify_terminattr_version")
     if not versions:
-        return None
+        result.result = "unset"
+        result.messages.append(
+            "verify_terminattr_version was not run as no "
+            "versions were givem"
+        )
+        return result
     try:
-        response = device.runCmds(1, ['show version detail'], 'json')
-    except jsonrpc.AppError:
-        return None
-    try:
-        if response[0]['details']['packages']['TerminAttr-core']['version'] in versions:
-            return True
-        return False
-    except KeyError:
-        return None
+        response = device.session.runCmds(1, ['show version detail'], 'json')
+        response_data = response[0]['details']['packages']['TerminAttr-core']['version']
+        if response_data in versions:
+            result.result = 'success'
+        else:
+            result.result = 'failure'
+            result.messages.append(f'device is running TerminAttr version {response_data} and is not in the allowed list')
+    except (jsonrpc.AppError, KeyError) as e:
+        result.messages.append(str(e))
+        result.result = 'error'
+    return result
 
-def verify_eos_extensions(device, enable_password):
+def verify_eos_extensions(device: InventoryDevice) -> TestResult:
     """
     Verifies all EOS extensions installed on the device are enabled for boot persistence.
 
@@ -70,7 +98,7 @@ def verify_eos_extensions(device, enable_password):
 
     """
     try:
-        response = device.runCmds(1, ['show extensions', 'show boot-extensions'], 'json')
+        response = device.session.runCmds(1, ['show extensions', 'show boot-extensions'], 'json')
     except jsonrpc.AppError:
         return None
     installed_extensions = []
@@ -93,92 +121,105 @@ def verify_eos_extensions(device, enable_password):
     except KeyError:
         return None
 
-def verify_field_notice_44_resolution(device, enable_password):
+
+def verify_field_notice_44_resolution(device: InventoryDevice) -> TestResult:
     """
     Verifies the device is using an Aboot version that fix the bug discussed
     in the field notice 44 (Aboot manages system settings prior to EOS initialization).
 
     Args:
-        device (jsonrpclib.jsonrpc.ServerProxy): Instance of the class jsonrpclib.jsonrpc.ServerProxy with the uri f'https://{username}:{password}@{ip}/command-api'.
-        enable_password (str): Enable password.
+        device (InventoryDevice): InventoryDevice instance containing all devices information.
 
     Returns:
-        bool: `True` if the device is using an Aboot version that fix the bug discussed
-        in the field notice 44 or if the device model is not affected.
-        `False` otherwise.
-
+        TestResult instance with
+        * result = "unset" if test has not been executed
+        * result = "success" if aboot is running valid version
+        * result = "failure" otherwise.
+        * result = "error" if any exception is caught
     """
+    result = TestResult(host=str(device.host),
+                        test="verify_field_notice_44_resolution")
     try:
-        response = device.runCmds(1, ['show version detail'], 'json')
-    except jsonrpc.AppError:
-        return None
-    devices = ['DCS-7010T-48',
-               'DCS-7010T-48-DC',
-               'DCS-7050TX-48',
-               'DCS-7050TX-64',
-               'DCS-7050TX-72',
-               'DCS-7050TX-72Q',
-               'DCS-7050TX-96',
-               'DCS-7050TX2-128',
-               'DCS-7050SX-64',
-               'DCS-7050SX-72',
-               'DCS-7050SX-72Q',
-               'DCS-7050SX2-72Q',
-               'DCS-7050SX-96',
-               'DCS-7050SX2-128',
-               'DCS-7050QX-32S',
-               'DCS-7050QX2-32S',
-               'DCS-7050SX3-48YC12',
-               'DCS-7050CX3-32S',
-               'DCS-7060CX-32S',
-               'DCS-7060CX2-32S',
-               'DCS-7060SX2-48YC6',
-               'DCS-7160-48YC6',
-               'DCS-7160-48TC6',
-               'DCS-7160-32CQ',
-               'DCS-7280SE-64',
-               'DCS-7280SE-68',
-               'DCS-7280SE-72',
-               'DCS-7150SC-24-CLD',
-               'DCS-7150SC-64-CLD',
-               'DCS-7020TR-48',
-               'DCS-7020TRA-48',
-               'DCS-7020SR-24C2',
-               'DCS-7020SRG-24C2',
-               'DCS-7280TR-48C6',
-               'DCS-7280TRA-48C6',
-               'DCS-7280SR-48C6',
-               'DCS-7280SRA-48C6',
-               'DCS-7280SRAM-48C6',
-               'DCS-7280SR2K-48C6-M',
-               'DCS-7280SR2-48YC6',
-               'DCS-7280SR2A-48YC6',
-               'DCS-7280SRM-40CX2',
-               'DCS-7280QR-C36',
-               'DCS-7280QRA-C36S']
-    variants = ['-SSD-F',
-                '-SSD-R',
-                '-M-F',
-                '-M-R',
-                '-F',
-                '-R']
-    try:
+        response = device.session.runCmds(1, ['show version detail'], 'json')
+        devices = ['DCS-7010T-48',
+                'DCS-7010T-48-DC',
+                'DCS-7050TX-48',
+                'DCS-7050TX-64',
+                'DCS-7050TX-72',
+                'DCS-7050TX-72Q',
+                'DCS-7050TX-96',
+                'DCS-7050TX2-128',
+                'DCS-7050SX-64',
+                'DCS-7050SX-72',
+                'DCS-7050SX-72Q',
+                'DCS-7050SX2-72Q',
+                'DCS-7050SX-96',
+                'DCS-7050SX2-128',
+                'DCS-7050QX-32S',
+                'DCS-7050QX2-32S',
+                'DCS-7050SX3-48YC12',
+                'DCS-7050CX3-32S',
+                'DCS-7060CX-32S',
+                'DCS-7060CX2-32S',
+                'DCS-7060SX2-48YC6',
+                'DCS-7160-48YC6',
+                'DCS-7160-48TC6',
+                'DCS-7160-32CQ',
+                'DCS-7280SE-64',
+                'DCS-7280SE-68',
+                'DCS-7280SE-72',
+                'DCS-7150SC-24-CLD',
+                'DCS-7150SC-64-CLD',
+                'DCS-7020TR-48',
+                'DCS-7020TRA-48',
+                'DCS-7020SR-24C2',
+                'DCS-7020SRG-24C2',
+                'DCS-7280TR-48C6',
+                'DCS-7280TRA-48C6',
+                'DCS-7280SR-48C6',
+                'DCS-7280SRA-48C6',
+                'DCS-7280SRAM-48C6',
+                'DCS-7280SR2K-48C6-M',
+                'DCS-7280SR2-48YC6',
+                'DCS-7280SR2A-48YC6',
+                'DCS-7280SRM-40CX2',
+                'DCS-7280QR-C36',
+                'DCS-7280QRA-C36S']
+        variants = ['-SSD-F',
+                    '-SSD-R',
+                    '-M-F',
+                    '-M-R',
+                    '-F',
+                    '-R']
+
         model = response[0]['modelName']
         for variant in variants:
             model = model.replace(variant, '')
         if model not in devices:
-            return True
+            result.result = 'unset'
+            result.messages.append('device is not impacted by FN044')
+
         for component in response[0]['details']['components']:
             if component['name'] == 'Aboot':
                 aboot_version = component['version'].split('-')[2]
+        result.result = 'success'
         if aboot_version.startswith('4.0.') and int(aboot_version.split('.')[2]) < 7:
-            return False
-        if aboot_version.startswith('4.1.') and int(aboot_version.split('.')[2]) < 1:
-            return False
-        if aboot_version.startswith('6.0.') and int(aboot_version.split('.')[2]) < 9:
-            return False
-        if aboot_version.startswith('6.1.') and int(aboot_version.split('.')[2]) < 7:
-            return False
-        return True
-    except KeyError:
-        return None
+            result.result = 'failure'
+            result.messages.append(
+                f'device is running incorrect version of aboot ({aboot_version})')
+        elif aboot_version.startswith('4.1.') and int(aboot_version.split('.')[2]) < 1:
+            result.result = 'failure'
+            result.messages.append(
+                f'device is running incorrect version of aboot ({aboot_version})')
+        elif aboot_version.startswith('6.0.') and int(aboot_version.split('.')[2]) < 9:
+            result.result = 'failure'
+            result.messages.append(
+                f'device is running incorrect version of aboot ({aboot_version})')
+        elif aboot_version.startswith('6.1.') and int(aboot_version.split('.')[2]) < 7:
+            result.result = 'failure'
+            result.messages.append(
+                f'device is running incorrect version of aboot ({aboot_version})')
+    except (jsonrpc.AppError, KeyError) as e:
+        result.messages.append(str(e))
+        result.result = 'error'
+    return result
