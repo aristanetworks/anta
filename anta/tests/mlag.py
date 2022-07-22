697ec4aa69e1c68bacdb2230f012788350085fd1
Guillaume Mulocher <gmulocher@arista.com>
2022-07-22 18:34:15 +0200

    refactor(tests): update mlag tests to support new structures (#80)

--- anta/tests/mlag.py
+++ anta/tests/mlag.py
@@ -2,92 +2,135 @@
 Test functions related to Multi-Chassis LAG
 """
 from jsonrpclib import jsonrpc
+from anta.inventory.models import InventoryDevice
+from anta.result_manager.models import TestResult
 
-def verify_mlag_status(device, enable_password):
 
+def verify_mlag_status(device: InventoryDevice) -> TestResult:
     """
     Verifies the MLAG status:
     state is active, negotiation status is connected, local int is up, peer link is up.
 
     Args:
-        device (jsonrpclib.jsonrpc.ServerProxy): Instance of the class jsonrpclib.jsonrpc.ServerProxy with the uri f'https://{username}:{password}@{ip}/command-api'.
-        enable_password (str): Enable password.
+        device (InventoryDevice): InventoryDevice instance containing all devices information.
 
     Returns:
-        bool: `True` if the MLAG status is OK.
-        `False` otherwise.
+        TestResult instance with
+        * result = "unset" if test has not been executed
+        * result = "success" if the MLAG status is OK
+        * result = "failure" otherwise.
+        * result = "error" if any exception is caught
 
     """
-    try:
-        response = device.runCmds(1, ['show mlag'], 'json')
-    except jsonrpc.AppError:
-        return None
-    try:
-        if response[0]['state'] == 'disabled':
-            return None
-        if response[0]['state'] != 'active':
-            return False
-        if response[0]['negStatus'] != 'connected':
-            return False
-        if response[0]['localIntfStatus'] != 'up':
-            return False
-        if response[0]['peerLinkStatus'] != 'up':
-            return False
-        return True
-    except KeyError:
-        return None
+    result = TestResult(host=str(device.host), test="verify_mlag_status")
 
-def verify_mlag_interfaces(device, enable_password):
+    try:
+        response = device.session.runCmds(1, ["show mlag"], "json")
+
+        if response[0]["state"] == "disabled":
+            result.result = "unset"
+            result.messages.append("MLAG is disabled")
+        elif (
+            response[0]["state"] != "active"
+            or response[0]["negStatus"] != "connected"
+            or response[0]["localIntfStatus"] != "up"
+            or response[0]["peerLinkStatus"] != "up"
+        ):
+            result.result = "failure"
+            result.messages.append(f"MLAG status is not OK: {response[0]}")
+        else:
+            result.result = "success"
+
+    except (jsonrpc.AppError, KeyError) as e:
+        result.messages.append(str(e))
+        result.result = "error"
+
+    return result
+
+
+def verify_mlag_interfaces(device: InventoryDevice) -> TestResult:
     """
     Verifies there is no inactive or active-partial MLAG interfaces.
 
     Args:
-        device (jsonrpclib.jsonrpc.ServerProxy): Instance of the class jsonrpclib.jsonrpc.ServerProxy with the uri f'https://{username}:{password}@{ip}/command-api'.
-        enable_password (str): Enable password.
+        device (InventoryDevice): InventoryDevice instance containing all devices information.
 
     Returns:
-        bool: `True` if there is no inactive or active-partial MLAG interfaces.
-        `False` otherwise.
+        TestResult instance with
+        * result = "unset" if test has not been executed
+        * result = "success" if there is no inactive or active-partial MLAG interfaces.
+        * result = "failure" otherwise.
+        * result = "error" if any exception is caught
 
     """
-    try:
-        response = device.runCmds(1, ['show mlag'], 'json')
-    except jsonrpc.AppError:
-        return None
-    try:
-        if response[0]['state'] == 'disabled':
-            return None
-        if response[0]['mlagPorts']['Inactive'] != 0:
-            return False
-        if response[0]['mlagPorts']['Active-partial'] != 0:
-            return False
-        return True
-    except KeyError:
-        return None
+    result = TestResult(host=str(device.host), test="verify_mlag_interfaces")
 
-def verify_mlag_config_sanity(device, enable_password):
+    try:
+        response = device.session.runCmds(1, ["show mlag"], "json")
+
+        if response[0]["state"] == "disabled":
+            result.result = "unset"
+            result.messages.append("MLAG is disabled")
+        elif (
+            response[0]["mlagPorts"]["Inactive"] != 0
+            or response[0]["mlagPorts"]["Active-partial"] != 0
+        ):
+            result.result = "failure"
+            result.messages.append(f"MLAG status is not OK: {response[0]['mlagPorts']}")
+        else:
+            result.result = "success"
+
+    except (jsonrpc.AppError, KeyError) as e:
+        result.messages.append(str(e))
+        result.result = "error"
+
+    return result
+
+
+def verify_mlag_config_sanity(device: InventoryDevice) -> TestResult:
     """
-    Verifies there is no MLAG config-sanity warnings.
+    Verifies there is no MLAG config-sanity inconsistencies.
 
     Args:
-        device (jsonrpclib.jsonrpc.ServerProxy): Instance of the class jsonrpclib.jsonrpc.ServerProxy with the uri f'https://{username}:{password}@{ip}/command-api'.
-        enable_password (str): Enable password.
+        device (InventoryDevice): InventoryDevice instance containing all devices information.
 
     Returns:
-        bool: `True` if there is no MLAG config-sanity warnings.
-        `False` otherwise.
+        TestResult instance with
+        * result = "unset" if test has not been executed
+        * result = "success" if there is no MLAG config-sanity inconsistencies
+        * result = "failure" otherwise.
+        * result = "error" if any exception is caught
+
     """
+    result = TestResult(host=str(device.host), test="verify_mlag_config_sanity")
+
     try:
-        response = device.runCmds(1, ['show mlag config-sanity'],'json')
-    except jsonrpc.AppError:
-        return None
-    try:
-        if response[0]['response']['mlagActive'] is False:
-            # MLAG isn't running
-            return None
-        if len(response[0]['response']['globalConfiguration']) > 0 or \
-            len(response[0]['response']['interfaceConfiguration']) > 0:
-            return False
-        return True
-    except KeyError:
-        return None
+        response = device.session.runCmds(1, ["show mlag config-sanity"], "json")
+
+        if response[0]["response"]["mlagActive"] is False:
+            # MLAG is not running
+            result.result = "unset"
+            result.messages.append("MLAG is disabled")
+        elif (
+            len(response[0]["response"]["globalConfiguration"]) > 0
+            or len(response[0]["response"]["interfaceConfiguration"]) > 0
+        ):
+            result.result = "failure"
+            if len(response[0]["response"]["globalConfiguration"]) > 0:
+                result.messages.append(
+                    "MLAG config-sanity returned some Global inconsistencies: "
+                    f"{response[0]['response']['globalConfiguration']}"
+                )
+            if len(response[0]["response"]["interfaceConfiguration"]) > 0:
+                result.messages.append(
+                    "MLAG config-sanity returned some Interface inconsistencies: "
+                    f"{response[0]['response']['interfaceConfiguration']}"
+                )
+        else:
+            result.result = "success"
+
+    except (jsonrpc.AppError, KeyError) as e:
+        result.messages.append(str(e))
+        result.result = "error"
+
+    return result
