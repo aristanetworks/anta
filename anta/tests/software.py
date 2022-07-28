3b6a51be36eb4bbdaed86bd882c1c125b1c285b7
gmuloc <gmulocher@arista.com>
2022-07-28 14:04:32 +0200

    refactor: refactor software.py tests

--- anta/tests/software.py
+++ anta/tests/software.py
@@ -1,11 +1,16 @@
 """
 Test functions related to the EOS software
 """
+import inspect
+import logging
+import socket
 from typing import List
 
 from jsonrpclib import jsonrpc
 from anta.inventory.models import InventoryDevice
 from anta.result_manager.models import TestResult
+
+logger = logging.getLogger(__name__)
 
 
 def verify_eos_version(
@@ -27,20 +32,27 @@ def verify_eos_version(
         * result = "error" if any exception is caught
 
     """
-    result = TestResult(host=str(device.host), test="verify_eos_version")
+    function_name = inspect.stack()[0][3]
+    logger.debug(f"Start {function_name} check for host {device.host}")
+    result = TestResult(host=str(device.host), test=function_name)
     if not versions:
         result.is_skipped("verify_eos_version was not run as no versions were given")
         return result
 
     try:
         response = device.session.runCmds(1, ["show version"], "json")
+        logger.debug(f"query result is: {response}")
+
         if response[0]["version"] in versions:
             result.is_success()
         else:
             result.is_failure(
-                f'device is running version {response[0]["version"]} not in expected version'
+                f'device is running version {response[0]["version"]} not in expected versions: {versions}'
             )
-    except (jsonrpc.AppError, KeyError) as e:
+    except (jsonrpc.AppError, KeyError, socket.timeout) as e:
+        logger.error(
+            f"exception raised for {inspect.stack()[0][3]} -  {device.host}: {str(e)}"
+        )
         result.is_error(str(e))
     return result
 
@@ -64,7 +76,9 @@ def verify_terminattr_version(
         * result = "error" if any exception is caught
 
     """
-    result = TestResult(host=str(device.host), test="verify_terminattr_version")
+    function_name = inspect.stack()[0][3]
+    logger.debug(f"Start {function_name} check for host {device.host}")
+    result = TestResult(host=str(device.host), test=function_name)
     if not versions:
         result.is_skipped(
             "verify_terminattr_version was not run as no versions were given"
@@ -72,14 +86,19 @@ def verify_terminattr_version(
         return result
     try:
         response = device.session.runCmds(1, ["show version detail"], "json")
+        logger.debug(f"query result is: {response}")
+
         response_data = response[0]["details"]["packages"]["TerminAttr-core"]["version"]
         if response_data in versions:
             result.is_success()
         else:
             result.is_failure(
-                f"device is running TerminAttr version {response_data} and is not in the allowed list"
+                f"device is running TerminAttr version {response_data} and is not in the allowed list: {versions}"
             )
-    except (jsonrpc.AppError, KeyError) as e:
+    except (jsonrpc.AppError, KeyError, socket.timeout) as e:
+        logger.error(
+            f"exception raised for {inspect.stack()[0][3]} -  {device.host}: {str(e)}"
+        )
         result.is_error(str(e))
     return result
 
@@ -99,11 +118,15 @@ def verify_eos_extensions(device: InventoryDevice) -> TestResult:
         * result = "error" if any exception is caught
 
     """
-    result = TestResult(host=str(device.host), test="verify_eos_extensions")
+    function_name = inspect.stack()[0][3]
+    logger.debug(f"Start {function_name} check for host {device.host}")
+    result = TestResult(host=str(device.host), test=function_name)
     try:
         response = device.session.runCmds(
             1, ["show extensions", "show boot-extensions"], "json"
         )
+        logger.debug(f"query result is: {response}")
+
         installed_extensions = []
         boot_extensions = []
         for extension in response[0]["extensions"]:
@@ -117,12 +140,16 @@ def verify_eos_extensions(device: InventoryDevice) -> TestResult:
                 boot_extensions.append(extension)
         installed_extensions.sort()
         boot_extensions.sort()
-        result.is_failure(
-            f"Missing EOS extensions: installed {installed_extensions} / configured: {boot_extensions}"
-        )
         if installed_extensions == boot_extensions:
             result.is_success()
-    except (jsonrpc.AppError, KeyError) as e:
+        else:
+            result.is_failure(
+                f"Missing EOS extensions: installed {installed_extensions} / configured: {boot_extensions}"
+            )
+    except (jsonrpc.AppError, KeyError, socket.timeout) as e:
+        logger.error(
+            f"exception raised for {inspect.stack()[0][3]} -  {device.host}: {str(e)}"
+        )
         result.is_error(str(e))
     return result
 
@@ -144,9 +171,19 @@ def verify_field_notice_44_resolution(device: InventoryDevice) -> TestResult:
         * result = "failure" otherwise.
         * result = "error" if any exception is caught
     """
-    result = TestResult(host=str(device.host), test="verify_field_notice_44_resolution")
+    function_name = inspect.stack()[0][3]
+    logger.debug(f"Start {function_name} check for host {device.host}")
+    result = TestResult(host=str(device.host), test=function_name)
+
+    if device.hw_model == "cEOSLab":
+        result.is_skipped(
+            f"{function_name} test is not supported on {device.hw_model}."
+        )
+        return result
     try:
         response = device.session.runCmds(1, ["show version detail"], "json")
+        logger.debug(f"query result is: {response}")
+
         devices = [
             "DCS-7010T-48",
             "DCS-7010T-48-DC",
@@ -200,6 +237,7 @@ def verify_field_notice_44_resolution(device: InventoryDevice) -> TestResult:
             model = model.replace(variant, "")
         if model not in devices:
             result.is_skipped("device is not impacted by FN044")
+            return result
 
         for component in response[0]["details"]["components"]:
             if component["name"] == "Aboot":
