ea2f3a79d5d0187e43af4b1c0a16e1840ee73af0
Thomas Grimonet <tgrimonet@arista.com>
2022-07-29 18:03:37 +0200

    refactor(tests): Update profiles to use logging and ignore virtual eos (#96)
    * refactor(tests): Update profiles to use logging and ignore virtual eos
    
    * fix(tests): Use decorator to skip HW

--- anta/tests/profiles.py
+++ anta/tests/profiles.py
@@ -1,11 +1,19 @@
 """
 Test functions related to ASIC profiles
 """
+import inspect
+import logging
+import socket
+
 from jsonrpclib import jsonrpc
+from anta.decorators import skip_on_platforms
 from anta.inventory.models import InventoryDevice
 from anta.result_manager.models import TestResult
 
+logger = logging.getLogger(__name__)
 
+
+@skip_on_platforms(["cEOSLab", "VEOS-LAB"])
 def verify_unified_forwarding_table_mode(
     device: InventoryDevice, mode: str
 ) -> TestResult:
@@ -25,7 +33,9 @@ def verify_unified_forwarding_table_mode(
         * result = "failure" otherwise.
         * result = "error" if any exception is caught
     """
-    result = TestResult(host=str(device.host), test="verify_vxlan")
+    function_name = inspect.stack()[0][3]
+    logger.debug(f"Start {function_name} check for host {device.host}")
+    result = TestResult(host=str(device.host), test=function_name)
     if not mode:
         result.is_skipped(
             "verify_unified_forwarding_table_mode was not run as no mode was given"
@@ -35,6 +45,7 @@ def verify_unified_forwarding_table_mode(
         response = device.session.runCmds(
             1, ["show platform trident forwarding-table partition"], "json"
         )
+        logger.debug(f'query result is: {response}')
         response_data = response[0]["uftMode"]
         if response_data == mode:
             result.is_success()
@@ -42,11 +53,14 @@ def verify_unified_forwarding_table_mode(
             result.is_failure(
                 f"device is not running correct UFT mode (expected: {mode} / running: {response_data})"
             )
-    except (jsonrpc.AppError, KeyError) as e:
+    except (jsonrpc.AppError, KeyError, socket.timeout) as e:
+        logger.error(
+            f'exception raised for {inspect.stack()[0][3]} -  {device.host}: {str(e)}')
         result.is_error(str(e))
     return result
 
 
+@skip_on_platforms(["cEOSLab", "VEOS-LAB"])
 def verify_tcam_profile(device: InventoryDevice, profile: str) -> TestResult:
 
     """
@@ -64,12 +78,15 @@ def verify_tcam_profile(device: InventoryDevice, profile: str) -> TestResult:
         * result = "failure" otherwise.
         * result = "error" if any exception is caught
     """
-    result = TestResult(host=str(device.host), test="verify_vxlan")
+    function_name = inspect.stack()[0][3]
+    logger.debug(f"Start {function_name} check for host {device.host}")
+    result = TestResult(host=str(device.host), test=function_name)
     if not profile:
         result.is_skipped("verify_tcam_profile was not run as no profile was given")
         return result
     try:
         response = device.session.runCmds(1, ["show hardware tcam profile"], "json")
+        logger.debug(f'query result is: {response}')
         if (
             response[0]["pmfProfiles"]["FixedSystem"]["status"]
             == response[0]["pmfProfiles"]["FixedSystem"]["config"]
@@ -79,6 +96,8 @@ def verify_tcam_profile(device: InventoryDevice, profile: str) -> TestResult:
             result.is_failure(
                 f'Incorrect profile configured on device: {response[0]["pmfProfiles"]["FixedSystem"]["status"]}'
             )
-    except (jsonrpc.AppError, KeyError) as e:
+    except (jsonrpc.AppError, KeyError, socket.timeout) as e:
+        logger.error(
+            f'exception raised for {inspect.stack()[0][3]} -  {device.host}: {e.__class__.__name__} - {str(e)}')
         result.is_error(str(e))
     return result
