449831047630badf39872f418e87cbb4e68aad24
Thomas Grimonet <tgrimonet@arista.com>
2022-07-29 18:07:09 +0200

    refactor(tests): Update routing.generic with logging (#105)
    * refactor(tests): Update routing.generic with logging
    
    * refactor(tests): Update routing.ospf with logging
    
    * make(gitignore): Ignore vim swap files

--- anta/tests/routing/ospf.py
+++ anta/tests/routing/ospf.py
@@ -1,9 +1,15 @@
 """
 OSPF test functions
 """
+import inspect
+import socket
+import logging
+
 from jsonrpclib import jsonrpc
 from anta.inventory.models import InventoryDevice
 from anta.result_manager.models import TestResult
+
+logger = logging.getLogger(__name__)
 
 
 def verify_ospf_state(device: InventoryDevice) -> TestResult:
@@ -20,16 +26,25 @@ def verify_ospf_state(device: InventoryDevice) -> TestResult:
         * result = "failure" otherwise.
         * result = "error" if any exception is caught
     """
-    result = TestResult(host=str(device.host), test="verify_ospf_state")
+    function_name = inspect.stack()[0][3]
+    logger.debug(f"Start {function_name} check for host {device.host}")
+    result = TestResult(host=str(device.host), test=function_name)
     try:
         response = device.session.runCmds(
             1, ["show ip ospf neighbor | exclude FULL|Address"], "text"
         )
+        logger.debug(f'query result is: {response}')
+        if len(response[0]["output"]) == 0:
+            result.is_skipped('no OSPF neighbor found')
+            return result
         if response[0]["output"].count("\n") == 0:
             result.is_success()
         else:
             result.is_failure("Some neighbors are not correctly configured.")
-    except (jsonrpc.AppError, KeyError) as e:
+    except (jsonrpc.AppError, KeyError, socket.timeout) as e:
+        logger.error(
+            f'exception raised for {inspect.stack()[0][3]} -  {device.host}: {str(e)}')
+
         result.is_error(str(e))
     return result
 
@@ -50,7 +65,9 @@ def verify_ospf_count(device: InventoryDevice, number: int) -> TestResult:
         * result = "failure" otherwise.
         * result = "error" if any exception is caught
     """
-    result = TestResult(host=str(device.host), test="verify_ospf_count")
+    function_name = inspect.stack()[0][3]
+    logger.debug(f"Start {function_name} check for host {device.host}")
+    result = TestResult(host=str(device.host), test=function_name)
     if not number:
         result.is_skipped(
             "verify_igmp_snooping_vlans was not run as no number was given"
@@ -60,6 +77,10 @@ def verify_ospf_count(device: InventoryDevice, number: int) -> TestResult:
         response = device.session.runCmds(
             1, ["show ip ospf neighbor | exclude  Address"], "text"
         )
+        logger.debug(f'query result is: {response}')
+        if len(response[0]["output"]) == 0:
+            result.is_skipped('no OSPF neighbor found')
+            return result
         response_data = response[0]["output"].count("FULL")
         if response_data.count("FULL") == number:
             result.is_success()
@@ -67,6 +88,9 @@ def verify_ospf_count(device: InventoryDevice, number: int) -> TestResult:
             result.is_failure(
                 f'device has {response_data.count("FULL")} neighbors (expected {number}'
             )
-    except (jsonrpc.AppError, KeyError) as e:
+    except (jsonrpc.AppError, KeyError, socket.timeout) as e:
+        logger.error(
+            f'exception raised for {inspect.stack()[0][3]} -  {device.host}: {str(e)}')
+
         result.is_error(str(e))
     return result
