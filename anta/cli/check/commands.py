06f515ce87bfe23206dcb3865f6dfcc8c214d3f4
Thomas Grimonet <titom73@users.noreply.github.com>
2023-01-26 10:48:25 +0000

    feat(cli): Implement regexp search and --skip-error trigger

--- anta/cli/check/commands.py
+++ anta/cli/check/commands.py
@@ -3,6 +3,7 @@
 
 import logging
 import click
+import re
 
 from rich.console import Console
 from rich.theme import Theme
@@ -14,44 +15,7 @@ from .utils import check_run, display_table, display_json, display_list
 
 logger = logging.getLogger(__name__)
 
-# Command to run live check of the network and provides a human readable ouput.
-# Not to be used in a CI pipeline.
-#
-# Usage:
-#
-# Usage: cli.py check [OPTIONS]
-#
-# ANTA command to check network states
-#
-# Options:
-#   -i, --inventory PATH            Path to your inventory file[env var:
-#                                                               ANTA_CHECK_INVENTORY]
-#   -c, --catalog PATH              Path for tests catalog[env var:
-#                                                          ANTA_CHECK_CATALOG]
-#   -t, --tags TEXT                 List of tags
-#   --search TEXT                   Value to search in result
-#   --group-by[none | host | test]
-#   --log-level, --log[debug | info | warning | critical]
-#   --help                          Show this message and exit.
-#
-# Example:
-#
-# python anta/cli/cli.py check --tags spine --group-by host --search clab-fabric-avd-spine1:443 --log critical
-# ╭────────────────────────────────────────────────────────── Settings ──────────────────────────────────────────────────────────╮
-# │ Running check-devices with:                                                                                                  │
-# │               - Inventory: /home/tom/Projects/arista/ansible-arista-network-lab-automation/network-tests/anta-inventory.yml  │
-# │               - Tests catalog: /home/tom/Projects/arista/ansible-arista-network-lab-automation/network-tests/tests-bases.yml │
-# ╰──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────╯
-#                                                                                                         Summary per host
-# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━┳━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-# ┃ Host IP                    ┃ # of success ┃ # of skipped ┃ # of failure ┃ # of errors ┃ List of failed ortest case                                                                                                            ┃
-# ┡━━━━━━━━━━━━━━━━━━━━━━━━━━━━╇━━━━━━━━━━━━━━╇━━━━━━━━━━━━━━╇━━━━━━━━━━━━━━╇━━━━━━━━━━━━━╇━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┩
-# │ clab-fabric-avd-spine1:443 │ 17           │ 9            │ 5            │ 0           │ ['verify_interface_errors', 'verify_interface_discards', 'verify_interfaces_status', 'verify_vxlan', 'verify_bgp_ipv4_unicast_count'] │
-# └────────────────────────────┴──────────────┴──────────────┴──────────────┴─────────────┴───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
-#
-#
-
-@anta.command()
+@anta.command(no_args_is_help=True)
 @click.pass_context
 # Generic options
 @click.option('--inventory', '-i', show_envvar=True, prompt='Inventory path', help='Path to your inventory file', type=click.Path())
@@ -101,14 +65,16 @@ def check(ctx, inventory, catalog, display, tags, group_by, search, output, log_
 @click.option('--inventory', '-i', show_envvar=True, prompt='Inventory path', help='Path to your inventory file', type=click.Path())
 @click.option('--catalog', '-c', show_envvar=True, prompt='Path for tests catalog', help='Path for tests catalog', type=click.Path())
 @click.option('--tags', '-t', default='all', help='List of tags using coma as separator: tag1,tag2,tag3', type=str)
-@click.option('--search', default=None, help='Value to search in result. Can be test name or host name', type=str)
+@click.option('--search', default=".*", help='Regular expression to search in both name and test', type=str)
+@click.option('--skip-error/--no-skip-error', help='Hide tests in errors due to connectivity issue', default=False)
 @click.option('--log-level', '--log', default='warning', type=click.Choice(['debug', 'info', 'warning', 'critical'], case_sensitive=False))
-def ci(ctx, inventory, catalog, tags, search, log_level):
+def ci(ctx, inventory, catalog, tags, search, skip_error, log_level):
     """Execute network testing in context of CI by mimicing Pytest output"""
     custom_theme = Theme({
         "success": "green4",
         "skipped": "orange3",
-        "failure": "bold red"
+        "failure": "bold red",
+        "error": "dark_red"
     })
     console = Console(theme=custom_theme)
     results = check_run(
@@ -121,9 +87,11 @@ def ci(ctx, inventory, catalog, tags, search, log_level):
         tags=tags,
         loglevel=log_level
     )
-    console.print(f'searching for {search}')
+    regexp = re.compile(search)
     for line in results.get_results(output_format="list"):
-        if search in [line.test, line.name, None]:
+        if any(regexp.match(entry) for entry in [line.name, line.test]) and (
+            skip_error is False or line.result != 'error'
+        ):
             message = f" ({str(line.messages[0])})" if len(line.messages) > 0 else ''
             console.print(
                 f'{line.name} :: {line.test} :: [{line.result}]{line.result.upper()}[/{line.result}]{message}', highlight=False)
