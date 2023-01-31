a65d6852033dfca194e0c3ef106e5d730513371d
Thomas Grimonet <titom73@users.noreply.github.com>
2023-01-31 15:41:01 +0000

    cut(cli): remove unused command in check group

--- anta/cli/check/commands.py
+++ anta/cli/check/commands.py
@@ -25,53 +25,6 @@ logger = logging.getLogger(__name__)
 @click.command(no_args_is_help=True)
 @click.pass_context
 # Generic options
-@click.option('--catalog', '-c', show_envvar=True, prompt='Path for tests catalog', help='Path for tests catalog', type=click.File())
-@click.option('--tags', '-t', default='all', help='List of tags using coma as separator: tag1,tag2,tag3', type=str)
-@click.option('--display', default='table', type=click.Choice(['table', 'json', 'list'], case_sensitive=False), help='output format selection. default is table')
-# Options valid with --display table
-@click.option('--search', default=None, help='Value to search in result. Can be test name or host name', type=str)
-@click.option('--group-by', default='none', type=click.Choice(['none', 'host', 'test'], case_sensitive=False), help='Group result by test or host. default none')
-# Options valid with --display json
-@click.option('--output', '-o', default=None, help='Path to save output in json or list', type=click.Path())
-# Debug stuf
-@click.option('--log-level', '--log', default='warning', type=click.Choice(['debug', 'info', 'warning', 'critical'], case_sensitive=False))
-def check_default(ctx: click.Context, catalog: str, display: str, tags: str, group_by: str, search: str, output: str, log_level: str) -> bool:
-    """ANTA command to check network states"""
-    console = Console()
-    inventory = ctx.obj['inventory']
-
-    console.print(
-        Panel.fit(
-            f"Running check-devices with:\n\
-              - Inventory: {inventory}\n\
-              - Tests catalog: {catalog}",
-            title="[green]Settings",
-        )
-    )
-
-    results = check_run(
-        inventory=inventory,
-        catalog=catalog,
-        username=ctx.obj['username'],
-        password=ctx.obj['password'],
-        timeout=ctx.obj['timeout'],
-        enable_password=ctx.obj['enable_password'],
-        tags=tags,
-        loglevel=log_level
-    )
-    if display == 'table':
-        display_table(console=console, results=results, group_by=group_by, search=search)
-    elif display == 'json':
-        display_json(console=console, results=results, output_file=output)
-    elif display == 'list':
-        display_list(console=console, results=results, output_file=output)
-
-    return True
-
-
-@click.command(no_args_is_help=True)
-@click.pass_context
-# Generic options
 @click.option('--catalog', '-c', show_envvar=True, prompt='Path for tests catalog', help='Path for tests catalog', type=str)
 @click.option('--tags', '-t', default='all', help='List of tags using coma as separator: tag1,tag2,tag3', type=str)
 # Options valid with --display table
