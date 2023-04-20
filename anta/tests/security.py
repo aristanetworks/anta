50de75e2f2ef7f61492573b83b23fe2406057659
Carl Baillargeon <carl.baillargeon@arista.com>
2023-04-20 09:00:54 -0400

    Added security test cases and verify_snmp_ipv6_acl

--- anta/tests/security.py
+++ anta/tests/security.py
@@ -1,5 +1,5 @@
 """
-Test functions related to the EOS various SNMP settings
+Test functions related to the EOS various security settings
 """
 import logging
 
@@ -11,82 +11,79 @@ logger = logging.getLogger(__name__)
 
 
 @anta_test
-async def verify_snmp_agent(device: InventoryDevice, result: TestResult, vrf: str = "default") -> TestResult:
+async def verify_ssh_default_vrf(device: InventoryDevice, result: TestResult) -> TestResult:
     """
-    Verifies if the SNMP agent is enabled.
+    Verifies if the SSHD agent is disabled in the default vrf.
 
     Args:
         device (InventoryDevice): InventoryDevice instance containing all devices information.
-        vrf(str): VRF to verify. Default is "default".
 
     Returns:
         TestResult instance with
         * result = "unset" if the test has not been executed
-        * result = "success" if SNMP agent is enabled in the specified VRF
+        * result = "success" if SSHD agent is disabled in the default VRF
         * result = "failure" otherwise
         * result = "error" if any exception is caught
     """
-    if not vrf:
-        result.is_skipped(
-            "verify_snmp_agent did not run because vrf was not supplied"
-        )
-        return result
-
-    response = await device.session.cli(command="show snmp", ofmt="json")
+    response = await device.session.cli(command="show management ssh", ofmt="text")
     logger.debug(f"query result is: {response}")
 
-    if response["enabled"] and vrf in response["vrfs"]["snmpVrfs"]:
+    line = [line for line in response.split('\n') if line.startswith('SSHD status')][0]
+    status = line.split('is ')[1]
+    if status == "disabled":
         result.is_success()
     else:
-        result.is_failure(
-            "SNMP agent disabled: Either no communities and no users are configured, or no VRFs are configured."
-        )
+        result.is_failure(line)
     return result
 
 
 @anta_test
-async def verify_snmp_ipv4_acl(device: InventoryDevice, result: TestResult, number: int, vrf: str = "default") -> TestResult:
+async def verify_ssh_ipv4_acl(device: InventoryDevice, result: TestResult, number: int, vrf: str = "default") -> TestResult:
     """
-    Verifies if the SNMP agent has IPv4 ACL(s) configured.
+    Verifies if the SSHD agent has IPv4 ACL(s) configured.
 
     Args:
         device (InventoryDevice): InventoryDevice instance containing all devices information.
-        number(int): Expected number of SNMP IPv4 ACL(s).
+        number(int): Expected number of SSHD IPv4 ACL(s).
         vrf(str): VRF to verify. Default is "default".
 
     Returns:
         TestResult instance with
         * result = "unset" if the test has not been executed
-        * result = "success" if SNMP agent has IPv4 ACL(s) configured
+        * result = "success" if SSH agent has IPv4 ACL(s) configured
         * result = "failure" otherwise
         * result = "error" if any exception is caught
     """
     if not number or not vrf:
         result.is_skipped(
-            "verify_snmp_ipv4_acl did not run because number or vrf was not supplied"
+            "verify_ssh_ipv4_acl did not run because number or vrf was not supplied"
         )
         return result
 
-    response = await device.session.cli(command="show snmp ipv4 access-list summary", ofmt="json")
+    device.assert_enable_password_is_not_none("verify_ssh_ipv4_acl")
+
+    enable_cmd = {"cmd": "enable", "input": str(device.enable_password)}
+    commands = [enable_cmd, "show management ssh ip access-list summary"]
+    response = await device.session.cli(commands=commands, ofmt="json")
     logger.debug(f"query result is: {response}")
 
-    ipv4_acl_list = response["ipAclList"]["aclList"]
+    ipv4_acl_list = response[1]["ipAclList"]["aclList"]
     ipv4_acl_number = len(ipv4_acl_list)
     not_configured_acl_list = []
 
     if ipv4_acl_number != number:
         result.is_failure(
-            f"Expected {number} SNMP IPv4 ACL(s) and got {ipv4_acl_number}"
+            f"Expected {number} SSH IPv4 ACL(s) and got {ipv4_acl_number}"
         )
         return result
 
     for ipv4_acl in ipv4_acl_list:
-        if vrf not in ipv4_acl["configuredVrfs"] or vrf not in ipv4_acl["activeVrfs"]:
+        if vrf not in ipv4_acl["activeVrfs"]:
             not_configured_acl_list.append(ipv4_acl["name"])
 
     if not_configured_acl_list:
         result.is_failure(
-            f"SNMP IPv4 ACL(s) not configured or active in vrf {vrf}: {not_configured_acl_list}"
+            f"SSH IPv4 ACL(s) not configured or active in vrf {vrf}: {not_configured_acl_list}"
         )
 
     else:
@@ -95,48 +92,52 @@ async def verify_snmp_ipv4_acl(device: InventoryDevice, result: TestResult, numb
 
 
 @anta_test
-async def verify_snmp_ipv6_acl(device: InventoryDevice, result: TestResult, number: int, vrf: str = "default") -> TestResult:
+async def verify_ssh_ipv6_acl(device: InventoryDevice, result: TestResult, number: int, vrf: str = "default") -> TestResult:
     """
-    Verifies if the SNMP agent has IPv6 ACL(s) configured.
+    Verifies if the SSHD agent has IPv6 ACL(s) configured.
 
     Args:
         device (InventoryDevice): InventoryDevice instance containing all devices information.
-        number(int): Expected number of SNMP IPv6 ACL(s).
+        number(int): Expected number of SSHD IPv6 ACL(s).
         vrf(str): VRF to verify. Default is "default".
 
     Returns:
         TestResult instance with
         * result = "unset" if the test has not been executed
-        * result = "success" if SNMP agent has IPv6 ACL(s) configured
+        * result = "success" if SSH agent has IPv6 ACL(s) configured
         * result = "failure" otherwise
         * result = "error" if any exception is caught
     """
     if not number or not vrf:
         result.is_skipped(
-            "verify_snmp_ipv6_acl did not run because number or vrf was not supplied"
+            "verify_ssh_ipv6_acl did not run because number or vrf was not supplied"
         )
         return result
 
-    response = await device.session.cli(command="show snmp ipv6 access-list summary", ofmt="json")
+    device.assert_enable_password_is_not_none("verify_ssh_ipv6_acl")
+
+    enable_cmd = {"cmd": "enable", "input": str(device.enable_password)}
+    commands = [enable_cmd, "show management ssh ipv6 access-list summary"]
+    response = await device.session.cli(commands=commands, ofmt="json")
     logger.debug(f"query result is: {response}")
 
-    ipv6_acl_list = response["ipv6AclList"]["aclList"]
+    ipv6_acl_list = response[1]["ipv6AclList"]["aclList"]
     ipv6_acl_number = len(ipv6_acl_list)
     not_configured_acl_list = []
 
     if ipv6_acl_number != number:
         result.is_failure(
-            f"Expected {number} SNMP IPv6 ACL(s) and got {ipv6_acl_number}"
+            f"Expected {number} SSH IPv6 ACL(s) and got {ipv6_acl_number}"
         )
         return result
 
     for ipv6_acl in ipv6_acl_list:
-        if vrf not in ipv6_acl["configuredVrfs"] or vrf not in ipv6_acl["activeVrfs"]:
+        if vrf not in ipv6_acl["activeVrfs"]:
             not_configured_acl_list.append(ipv6_acl["name"])
 
     if not_configured_acl_list:
         result.is_failure(
-            f"SNMP IPv6 ACL(s) not configured or active in vrf {vrf}: {not_configured_acl_list}"
+            f"SSH IPv6 ACL(s) not configured or active in vrf {vrf}: {not_configured_acl_list}"
         )
 
     else:
