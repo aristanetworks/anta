e55f53a9a79e3181c6c96ecdd1c6fd7832a17412
gmuloc <gmulocher@arista.com>
2023-04-25 23:53:05 +0200

    Test: Turns out cast does not support annotations in 3.8

--- anta/tests/hardware.py
+++ anta/tests/hardware.py
@@ -1,13 +1,10 @@
 """
 Test functions related to the hardware or environement
 """
-
-# pylint: disable = too-few-public-methods
-
 from __future__ import annotations
 
 import logging
-from typing import Any, List, Optional, cast
+from typing import Any, Dict, List, Optional, cast
 
 from anta.decorators import skip_on_platforms
 from anta.models import AntaTest, AntaTestCommand
@@ -33,7 +30,7 @@ class VerifyTransceiversManufacturers(AntaTest):
         if not manufacturers:
             self.result.is_skipped(f"{self.__class__.name} was not run as no manufacturers were given")
         else:
-            command_output = cast(dict[str, dict[Any, Any]], self.instance_commands[0].output)
+            command_output = cast(Dict[str, Dict[Any, Any]], self.instance_commands[0].output)
             wrong_manufacturers = {interface: value["mfgName"] for interface, value in command_output["xcvrSlots"].items() if value["mfgName"] not in manufacturers}
             if not wrong_manufacturers:
                 self.result.is_success()
