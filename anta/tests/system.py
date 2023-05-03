bb145d1af9cceafef27f05a055cebbdf6800ac12
gmuloc <gmulocher@arista.com>
2023-05-03 13:03:49 +0200

    refactor(anta.tests): Move anta.tests.system to new structure

--- anta/tests/system.py
+++ anta/tests/system.py
@@ -1,277 +1,242 @@
 """
 Test functions related to system-level features and protocols
 """
-import logging
-from typing import Optional
+from __future__ import annotations
 
-from anta.inventory.models import InventoryDevice
-from anta.result_manager.models import TestResult
-from anta.tests import anta_test
+import logging
+from typing import Any, Dict, cast
+
+from anta.models import AntaTest, AntaTestCommand
 
 logger = logging.getLogger(__name__)
 
 
-@anta_test
-async def verify_uptime(device: InventoryDevice, result: TestResult, minimum: Optional[int] = None) -> TestResult:
+class VerifyUptime(AntaTest):
     """
     Verifies the device uptime is higher than a value.
-
-    Args:
-        device (InventoryDevice): InventoryDevice instance containing all devices information.
-        minimum (int): Minimum uptime in seconds.
-
-    Returns:
-        TestResult instance with
-        * result = "unset" if the test has not been executed
-        * result = "skipped" if the `minimum` parameter is  missing
-        * result = "success" if uptime is greater than minimun
-        * result = "failure" otherwise.
-        * result = "error" if any exception is caught
-
     """
-    if not minimum:
-        result.is_skipped("verify_uptime was not run as no minimum were given")
-        return result
 
-    response = await device.session.cli(command="show uptime", ofmt="json")
-    logger.debug(f"query result is: {response}")
-    if response["upTime"] > minimum:
-        result.is_success()
-    else:
-        result.is_failure(f"Uptime is {response['upTime']}")
+    name = "VerifyUptime"
+    description = "Verifies the device uptime is higher than a value."
+    categories = ["system"]
+    commands = [AntaTestCommand(command="show uptime")]
 
-    return result
+    @AntaTest.anta_test
+    def test(self, minimum: int = -1) -> None:
+        """
+        Run VerifyUptime validation
+        minimum (int): Minimum uptime in seconds.
+        """
+
+        command_output = cast(Dict[str, Dict[Any, Any]], self.instance_commands[0].output)
+
+        if minimum < 0:
+            self.result.is_skipped("VerifyUptime was not run as no minimum were given")
+            return
+
+        if cast(float, command_output["upTime"]) > minimum:
+            self.result.is_success()
+        else:
+            self.result.is_failure(f"Uptime is {command_output['upTime']}")
 
 
-@anta_test
-async def verify_reload_cause(device: InventoryDevice, result: TestResult) -> TestResult:
+class VerifyReloadCause(AntaTest):
     """
     Verifies the last reload of the device was requested by a user.
 
     Test considers the following messages as normal and will return success. Failure is for other messages
     * Reload requested by the user.
     * Reload requested after FPGA upgrade
-
-    Args:
-        device (InventoryDevice): InventoryDevice instance containing all devices information.
-
-    Returns:
-        TestResult instance with
-        * result = "unset" if the test has not been executed
-        * result = "success" if reload cause is standard
-        * result = "failure" otherwise.
-        * result = "error" if any exception is caught
-
     """
-    response = await device.session.cli(command="show reload cause", ofmt="json")
-    logger.debug(f"query result is: {response}")
-    if "resetCauses" not in response.keys() or len(response["resetCauses"]) == 0:
-        result.is_error("no reload cause available")
-        return result
 
-    response_data = response.get("resetCauses")[0].get("description")
-    if response_data in [
-        "Reload requested by the user.",
-        "Reload requested after FPGA upgrade",
-    ]:
-        result.is_success()
-    else:
-        result.is_failure(f"Reload cause is {response_data}")
+    name = "VerifyReloadCause"
+    description = "Verifies the device uptime is higher than a value."
+    categories = ["system"]
+    commands = [AntaTestCommand(command="show reload cause")]
 
-    return result
+    @AntaTest.anta_test
+    def test(self) -> None:
+        """
+        Run VerifyReloadCause validation
+        """
+
+        command_output = cast(Dict[str, Dict[Any, Any]], self.instance_commands[0].output)
+
+        if "resetCauses" not in command_output.keys() or len(command_output["resetCauses"]) == 0:
+            self.result.is_error("no reload cause available")
+            return
+
+        reset_causes = cast(list[dict[str, Any]], command_output["resetCauses"])
+        command_output_data = reset_causes[0].get("description")
+        if command_output_data in [
+            "Reload requested by the user.",
+            "Reload requested after FPGA upgrade",
+        ]:
+            self.result.is_success()
+        else:
+            self.result.is_failure(f"Reload cause is {command_output_data}")
 
 
-@anta_test
-async def verify_coredump(device: InventoryDevice, result: TestResult) -> TestResult:
+class VerifyCoredump(AntaTest):
     """
     Verifies there is no core file.
-
-    Args:
-        device (InventoryDevice): InventoryDevice instance containing all devices information.
-
-    Returns:
-        TestResult instance with
-        * result = "unset" if the test has not been executed
-        * result = "success" if device has no core-dump
-        * result = "failure" otherwise.
-        * result = "error" if any exception is caught
-
     """
-    device.assert_enable_password_is_not_none("verify_coredump")
 
-    response = await device.session.cli(
-        commands=[
-            {"cmd": "enable", "input": str(device.enable_password)},
-            "bash timeout 10 ls /var/core",
-        ],
-        ofmt="text",
-    )
-    logger.debug(f"query result is: {response}")
-    response_data = response[1]
-    if len(response_data) == 0:
-        result.is_success()
-    else:
-        result.is_failure(f"Core-dump(s) have been found: {response_data}")
+    name = "VerifyCoredump"
+    description = "Verifies there is no core file."
+    categories = ["system"]
+    commands = [AntaTestCommand(command="bash timeout 10 ls /var/core", ofmt="text")]
 
-    return result
+    @AntaTest.anta_test
+    def test(self) -> None:
+        """
+        Run VerifyCoredump validation
+        """
+        command_output = cast(str, self.instance_commands[0].output)
+
+        if len(command_output) == 0:
+            self.result.is_success()
+        else:
+            self.result.is_failure(f"Core-dump(s) have been found: {command_output}")
 
 
-@anta_test
-async def verify_agent_logs(device: InventoryDevice, result: TestResult) -> TestResult:
+class VerifyAgentLogs(AntaTest):
     """
     Verifies there is no agent crash reported on the device.
-
-    Args:
-        device (InventoryDevice): InventoryDevice instance containing all devices information.
-
-    Returns:
-        TestResult instance with
-        * result = "unset" if the test has not been executed
-        * result = "success" if there is no agent crash
-        * result = "failure" otherwise.
-        * result = "error" if any exception is caught
-
     """
-    response = await device.session.cli(command="show agent logs crash", ofmt="text")
-    logger.debug(f"query result is: {response}")
-    if len(response) == 0:
-        result.is_success()
-    else:
-        result.is_failure(f"device reported some agent crashes: {response}")
 
-    return result
+    name = "VerifyAgentLogs"
+    description = "Verifies there is no agent crash reported on the device."
+    categories = ["system"]
+    commands = [AntaTestCommand(command="show agent logs crash", ofmt="text")]
+
+    @AntaTest.anta_test
+    def test(self) -> None:
+        """
+        Run VerifyAgentLogs validation
+        """
+        command_output = cast(str, self.instance_commands[0].output)
+
+        if len(command_output) == 0:
+            self.result.is_success()
+        else:
+            self.result.is_failure(f"device reported some agent crashes: {command_output}")
 
 
-@anta_test
-async def verify_syslog(device: InventoryDevice, result: TestResult) -> TestResult:
+class VerifySyslog(AntaTest):
     """
-    Verifies the device had no syslog message with a severity of warning (or a more severe message)
-    during the last 7 days.
-
-    Args:
-        device (InventoryDevice): InventoryDevice instance containing all devices information.
-
-    Returns:
-        TestResult instance with
-        * result = "unset" if the test has not been executed
-        * result = "success" if syslog has no WARNING message
-        * result = "failure" otherwise.
-        * result = "error" if any exception is caught
+    Verifies the device had no syslog message with a severity of warning (or a more severe message) during the last 7 days.
     """
-    response = await device.session.cli(command="show logging last 7 days threshold warnings", ofmt="text")
-    logger.debug(f"query result is: {response}")
-    if len(response) == 0:
-        result.is_success()
-    else:
-        result.is_failure("Device has some log messages with a severity WARNING or higher")
 
-    return result
+    name = "VerifySyslog"
+    description = "Verifies the device had no syslog message with a severity of warning (or a more severe message) during the last 7 days."
+    categories = ["system"]
+    commands = [AntaTestCommand(command="show logging last 7 days threshold warnings", ofmt="text")]
+
+    @AntaTest.anta_test
+    def test(self) -> None:
+        """
+        Run VerifySyslog validation
+        """
+        command_output = cast(str, self.instance_commands[0].output)
+
+        if len(command_output) == 0:
+            self.result.is_success()
+        else:
+            self.result.is_failure("Device has some log messages with a severity WARNING or higher")
 
 
-@anta_test
-async def verify_cpu_utilization(device: InventoryDevice, result: TestResult) -> TestResult:
+class VerifyCPUUtilization(AntaTest):
     """
     Verifies the CPU utilization is less than 75%.
-
-    Args:
-        device (InventoryDevice): InventoryDevice instance containing all devices information.
-
-    Returns:
-        TestResult instance with
-        * result = "unset" if the test has not been executed
-        * result = "success" if CPU usage is lower than 75%
-        * result = "failure" otherwise.
-        * result = "error" if any exception is caught
     """
-    response = await device.session.cli(command="show processes top once", ofmt="json")
-    logger.debug(f"query result is: {response}")
-    response_data = response["cpuInfo"]["%Cpu(s)"]["idle"]
-    if response_data > 25:
-        result.is_success()
-    else:
-        result.is_failure(f"device reported a high CPU utilization ({response_data}%)")
 
-    return result
+    name = "VerifyCPUUtilization"
+    description = "Verifies the CPU utilization is less than 75%."
+    categories = ["system"]
+    commands = [AntaTestCommand(command="show processes top once")]
+
+    @AntaTest.anta_test
+    def test(self) -> None:
+        """
+        Run VerifyCPUUtilization validation
+        """
+        command_output = cast(Dict[str, Dict[Any, Any]], self.instance_commands[0].output)
+        command_output_data = command_output["cpuInfo"]["%Cpu(s)"]["idle"]
+
+        if command_output_data > 25:
+            self.result.is_success()
+        else:
+            self.result.is_failure(f"device reported a high CPU utilization ({command_output_data}%)")
 
 
-@anta_test
-async def verify_memory_utilization(device: InventoryDevice, result: TestResult) -> TestResult:
+class VerifyMemoryUtilization(AntaTest):
     """
-    Verifies the memory utilization is less than 75%.
-
-    Args:
-        device (InventoryDevice): InventoryDevice instance containing all devices information.
-
-    Returns:
-        TestResult instance with
-        * result = "unset" if the test has not been executed
-        * result = "success" if memory usage is lower than 75%
-        * result = "failure" otherwise.
-        * result = "error" if any exception is caught
+    Verifies the Memory utilization is less than 75%.
     """
-    response = await device.session.cli(command="show version", ofmt="json")
-    logger.debug(f"query result is: {response}")
-    memory_usage = float(response["memFree"]) / float(response["memTotal"])
-    if memory_usage > 0.25:
-        result.is_success()
-    else:
-        result.is_failure(f"device report a high memory usage: {memory_usage*100}%")
 
-    return result
+    name = "VerifyMemoryUtilization"
+    description = "Verifies the Memory utilization is less than 75%."
+    categories = ["system"]
+    commands = [AntaTestCommand(command="show version")]
+
+    @AntaTest.anta_test
+    def test(self) -> None:
+        """
+        Run VerifyMemoryUtilization validation
+        """
+        command_output = cast(Dict[str, Dict[Any, Any]], self.instance_commands[0].output)
+
+        memory_usage = float(cast(float, command_output["memFree"])) / float(cast(float, command_output["memTotal"]))
+        if memory_usage > 0.25:
+            self.result.is_success()
+        else:
+            self.result.is_failure(f"device report a high memory usage: {memory_usage*100}%")
 
 
-@anta_test
-async def verify_filesystem_utilization(device: InventoryDevice, result: TestResult) -> TestResult:
+class VerifyFileSystemUtilization(AntaTest):
     """
     Verifies each partition on the disk is used less than 75%.
-
-    Args:
-        device (InventoryDevice): InventoryDevice instance containing all devices information.
-
-    Returns:
-        TestResult instance with
-        * result = "unset" if the test has not been executed
-        * result = "success" if disk is used less than 75%
-        * result = "failure" otherwise.
-        * result = "error" if any exception is caught
     """
-    response = await device.session.cli(
-        commands=[
-            {"cmd": "enable", "input": device.enable_password},
-            "bash timeout 10 df -h",
-        ],
-        ofmt="text",
-    )
-    logger.debug(f"query result is: {response}")
-    result.is_success()
-    for line in response[1].split("\n")[1:]:
-        if "loop" not in line and len(line) > 0 and int(line.split()[4].replace("%", "")) > 75:
-            result.is_failure(f'mount point {line} is higher than 75% (reprted {int(line.split()[4].replace(" % ", ""))})')
 
-    return result
+    name = "VerifyFileSystemUtilization"
+    description = "Verifies the Memory utilization is less than 75%."
+    categories = ["system"]
+    commands = [AntaTestCommand(command="show processes top once", ofmt="text")]
+
+    @AntaTest.anta_test
+    def test(self) -> None:
+        """
+        Run VerifyFileSystemUtilization validation
+        """
+        command_output = cast(str, self.instance_commands[0].output)
+
+        self.result.is_success()
+
+        for line in command_output[1].split("\n")[1:]:
+            if "loop" not in line and len(line) > 0 and int(line.split()[4].replace("%", "")) > 75:
+                self.result.is_failure(f'mount point {line} is higher than 75% (reprted {int(line.split()[4].replace(" % ", ""))})')
 
 
-@anta_test
-async def verify_ntp(device: InventoryDevice, result: TestResult) -> TestResult:
+class VerifyNTP(AntaTest):
     """
     Verifies NTP is synchronised.
-
-    Args:
-        device (InventoryDevice): InventoryDevice instance containing all devices information.
-
-    Returns:
-        TestResult instance with
-        * result = "unset" if the test has not been executed
-        * result = "success" if synchronized with NTP server
-        * result = "failure" otherwise.
-        * result = "error" if any exception is caught
     """
-    response = await device.session.cli(command="show ntp status", ofmt="text")
-    logger.debug(f"query result is: {response}")
-    if response.split("\n")[0].split(" ")[0] == "synchronised":
-        result.is_success()
-    else:
-        data = response.split("\n")[0]
-        result.is_failure(f"not sync with NTP server ({data})")
 
-    return result
+    name = "VerifyNTP"
+    description = "Verifies NTP is synchronised."
+    categories = ["system"]
+    commands = [AntaTestCommand(command="show ntp status", ofmt="text")]
+
+    @AntaTest.anta_test
+    def test(self) -> None:
+        """
+        Run VerifyNTP validation
+        """
+        command_output = cast(str, self.instance_commands[0].output)
+
+        if command_output.split("\n")[0].split(" ")[0] == "synchronised":
+            self.result.is_success()
+        else:
+            data = command_output.split("\n")[0]
+            self.result.is_failure(f"not sync with NTP server ({data})")
