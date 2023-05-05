9caeff6e4e36ca4705f8d0a4bffd6ceaa5c771d9
Colin MacGiollaEain <43743234+colinmacgiolla@users.noreply.github.com>
2023-05-05 17:27:50 +0100

    refactor(anta.tests): Refactor TCAM and UFT profiles (#187)

--- anta/tests/profiles.py
+++ anta/tests/profiles.py
@@ -2,77 +2,57 @@
 Test functions related to ASIC profiles
 """
 import logging
+from typing import Any, Dict, cast
 
 from anta.decorators import skip_on_platforms
-from anta.inventory.models import InventoryDevice
-from anta.result_manager.models import TestResult
-from anta.tests import anta_test
+from anta.models import AntaTest, AntaTestCommand
 
 logger = logging.getLogger(__name__)
 
 
-@skip_on_platforms(["cEOSLab", "VEOS-LAB"])
-@anta_test
-async def verify_unified_forwarding_table_mode(device: InventoryDevice, result: TestResult, mode: str) -> TestResult:
+class VerifyUnifiedForwardingTableMode(AntaTest):
     """
     Verifies the device is using the expected Unified Forwarding Table mode.
-
-    Args:
-        device (InventoryDevice): InventoryDevice instance containing all devices information.
-        mode (str): The expected Unified Forwarding Table mode.
-
-    Returns:
-        TestResult instance with
-        * result = "unset" if the test has not been executed
-        * result = "mode" if the `mode` parameter is missing
-        * result = "success" if UFT mode is correct
-        * result = "failure" otherwise.
-        * result = "error" if any exception is caught
     """
-    if not mode:
-        result.is_skipped("verify_unified_forwarding_table_mode was not run as no mode was given")
-        return result
 
-    response = await device.session.cli(command="show platform trident forwarding-table partition", ofmt="json")
-    logger.debug(f"query result is: {response}")
-    response_data = response["uftMode"]
-    if response_data == mode:
-        result.is_success()
-    else:
-        result.is_failure(f"device is not running correct UFT mode (expected: {mode} / running: {response_data})")
+    name = "verify_unified_forwarding_table_mode"
+    description = ""
+    categories = ["profiles"]
+    commands = [AntaTestCommand(command="show platform trident forwarding-table partition", ofmt="json")]
 
-    return result
+    @skip_on_platforms(["cEOSLab", "vEOS-lab"])
+    @AntaTest.anta_test
+    def test(self, mode: Any = None) -> None:
+        if not mode:
+            self.result.is_skipped("verify_unified_forwarding_table_mode was not run as no mode was given")
+            return
+
+        command_output = cast(Dict[str, Dict[Any, Any]], self.instance_commands[0].output)
+        if command_output["uftMode"] == mode:
+            self.result.is_success()
+        else:
+            self.result.is_failure(f"Device is not running correct UFT mode (expected: {mode} / running: {command_output['uftMode']})")
 
 
-@skip_on_platforms(["cEOSLab", "VEOS-LAB"])
-@anta_test
-async def verify_tcam_profile(device: InventoryDevice, result: TestResult, profile: str) -> TestResult:
+class VerifyTcamProfile(AntaTest):
     """
-    Verifies the configured TCAM profile is the expected one.
-
-    Args:
-        device (InventoryDevice): InventoryDevice instance containing all devices information.
-        profile (str): The expected TCAM profile.0
-
-    Returns:
-        TestResult instance with
-        * result = "unset" if the test has not been executed
-        * result = "mode" if the `profile` parameter is missing
-        * result = "success" if TCAM profile is correct
-        * result = "failure" otherwise.
-        * result = "error" if any exception is caught
+    Verifies the device is using the configured TCAM profile.
     """
-    if not profile:
-        result.is_skipped("verify_tcam_profile was not run as no profile was given")
-        return result
 
-    response = await device.session.cli(command="show hardware tcam profile", ofmt="json")
-    logger.debug(f"query result is: {response}")
-    if (response["pmfProfiles"]["FixedSystem"]["status"] == response["pmfProfiles"]["FixedSystem"]["config"]) and (
-        response["pmfProfiles"]["FixedSystem"]["status"] == profile
-    ):
-        result.is_success()
-    else:
-        result.is_failure(f'Incorrect profile configured on device: {response["pmfProfiles"]["FixedSystem"]["status"]}')
+    name = "verify_tcam_profile"
+    description = "Verify that the assigned TCAM profile is actually running on the device"
+    categories = ["profiles"]
+    commands = [AntaTestCommand(command="show hardware tcam profile", ofmt="json")]
 
-    return result
+    @skip_on_platforms(["cEOSLab", "vEOS-lab"])
+    @AntaTest.anta_test
+    def test(self, profile: Any = None) -> None:
+        if not profile:
+            self.result.is_skipped("verify_tcam_profile was not run as no profile was given")
+            return
+
+        command_output = cast(Dict[str, Dict[Any, Any]], self.instance_commands[0].output)
+        if command_output["pmfProfiles"]["FixedSystem"]["status"] == command_output["pmfProfiles"]["FixedSystem"]["config"] == profile:
+            self.result.is_success()
+        else:
+            self.result.is_failure(f"Incorrect profile running on device: {command_output['pmfProfiles']['FixedSystem']['status']}")
---
+++ tests/units/anta_tests/profiles/data.py
@@ -0,0 +1,93 @@
+"""Test inputs for anta.tests.profiles"""
+
+from typing import Any, Dict, List
+
+INPUT_UFT_SETTING: List[Dict[str, Any]] = [
+    {
+        "name": "skipped",
+        "eos_data": [{}],
+        "side_effect": None,
+        "expected_result": "skipped",
+        "expected_messages": ["verify_unified_forwarding_table_mode was not run as no mode was given"]
+    },
+    {
+        "name": "success",
+        "eos_data": [
+            {
+                "uftMode": "2",
+                "urpfEnabled": False,
+                "chipModel": "bcm56870",
+                "l2TableSize": 163840,
+                "l3TableSize": 147456,
+                "lpmTableSize": 32768
+            }
+        ],
+        "side_effect": "2",
+        "expected_result": "success",
+        "expected_messages": []
+    },
+    {
+        "name": "failure",
+        "eos_data": [
+            {
+                "uftMode": "2",
+                "urpfEnabled": False,
+                "chipModel": "bcm56870",
+                "l2TableSize": 163840,
+                "l3TableSize": 147456,
+                "lpmTableSize": 32768
+            }
+        ],
+        "side_effect": "3",
+        "expected_result": "failure",
+        "expected_messages": ["Device is not running correct UFT mode (expected: 3 / running: 2)"]
+    },
+]
+
+INPUT_TCAM_PROFILE: List[Dict[str, Any]] = [
+    {
+        "name": "skipped",
+        "eos_data": [{}],
+        "side_effect": None,
+        "expected_result": "skipped",
+        "expected_messages": ["verify_tcam_profile was not run as no profile was given"]
+    },
+    {
+        "name": "success",
+        "eos_data": [
+            {
+                "pmfProfiles": {
+                    "FixedSystem": {
+                        "config": "test",
+                        "configType": "System Profile",
+                        "status": "test",
+                        "mode": "tcam"
+                    }
+                },
+                "lastProgrammingStatus": {}
+            }
+        ],
+        "side_effect": "test",
+        "expected_result": "success",
+        "expected_messages": []
+    },
+    {
+        "name": "failure",
+        "eos_data": [
+            {
+                "pmfProfiles": {
+                    "FixedSystem": {
+                        "config": "test",
+                        "configType": "System Profile",
+                        "status": "default",
+                        "mode": "tcam"
+                    }
+                },
+                "lastProgrammingStatus": {}
+            }
+        ],
+        "side_effect": "test",
+        "expected_result": "failure",
+        "expected_messages": ["Incorrect profile running on device: default"]
+    },
+]
