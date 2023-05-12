4e4e877cf1b32f7c9ca4a06d5ec620de135a9e3a
Colin MacGiollaEain <colin@arista.com>
2023-05-12 11:17:48 +0100

    Add FN72 test

--- anta/tests/field_notices.py
+++ anta/tests/field_notices.py
@@ -101,3 +101,72 @@ class VerifyFieldNotice44Resolution(AntaTest):
             self.result.is_failure(f"device is running incorrect version of aboot ({aboot_version})")
         elif aboot_version.startswith("6.1.") and int(aboot_version.split(".")[2]) < 7:
             self.result.is_failure(f"device is running incorrect version of aboot ({aboot_version})")
+
+
+class VerifyFieldNotice72Resolution(AntaTest):
+    """
+    Checks if the device is potentially exposed to Field Notice 72, and if the issue has been mitigated.
+
+    https://www.arista.com/en/support/advisories-notices/field-notice/17410-field-notice-0072
+    """
+
+    name = "VerifyFieldNotice72Resolution"
+    description = (
+        "Verifies if the device has exposeure to FN72, and if the issue has been mitigated"
+    )
+    categories = ["field notices", "software"]
+    commands = [AntaTestCommand(command="show version detail")]
+
+    # TODO maybe implement ONLY ON PLATFORMS instead
+    @skip_on_platforms(["cEOSLab"])
+    @AntaTest.anta_test
+    def test(self) -> None:  # type: ignore[override]
+        """Run VerifyFieldNotice72Resolution validation"""
+
+        command_output = cast(Dict[str, Dict[Any, Any]], self.instance_commands[0].output)
+
+        devices = ["DCS-7280SR3-48YC8", "DCS-7280SR3K-48YC8"]
+        variants = ["-SSD-F", "-SSD-R", "-M-F", "-M-R", "-F", "-R"]
+        model = cast(str, command_output["modelName"])
+
+        for variant in variants:
+            model = model.replace(variant, "")
+        if model not in devices:
+            self.result.is_skipped("Platform is not impacted by FN072")
+            return
+
+        serial = command_output["serialNumber"]
+        number = serial[3:7]
+
+        if "JPE" not in serial and "JAS" not in serial:
+            self.result.is_skipped("Device not exposed")
+            return
+
+        if model == "DCS-7280SR3-48YC8" and "JPE" in serial and number >= 2131:
+            self.result.is_skipped("Device not exposed")
+            return
+
+        if model == "DCS-7280SR3-48YC8" and "JAS" in serial and number >= 2041:
+            self.result.is_skipped("Device not exposed")
+            return
+
+        if model == "DCS-7280SR3K-48YC8" and "JPE" in serial and number >= 2134:
+            self.result.is_skipped("Device not exposed")
+            return
+
+        if model == "DCS-7280SR3K-48YC8" and "JAS" in serial and number >= 2041:
+            self.result.is_skipped("Device not exposed")
+            return
+
+        # Because each of the if checks above will return if taken, we only run the long
+        # check if we get this far
+        for entry in command_output["details"]["components"]:
+            if entry["name"] == "FixedSystemvrm1":
+                if int(entry["version"]) < 7:
+                    self.result.is_failure("Device is exposed to FN72")
+                else:
+                    self.result.is_success("FN72 is mitigated")
+                return
+        # We should never hit this point
+        self.result.is_error("Error in running test")
+        return
