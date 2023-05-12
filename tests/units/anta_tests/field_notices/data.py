54bebc0704f4ea98109fbbc755f5a124e6a1c7ce
Colin MacGiollaEain <colin@arista.com>
2023-05-12 11:34:38 +0100

    Add testcases

--- tests/units/anta_tests/field_notices/data.py
+++ tests/units/anta_tests/field_notices/data.py
@@ -107,3 +107,182 @@ INPUT_FIELD_NOTICE_44_RESOLUTION: List[Dict[str, Any]] = [
         "expected_messages": ["device is not impacted by FN044"],
     },
 ]
+
+INPUT_FIELD_NOTICE_72_RESOLUTION: List[Dict[str, Any]] = [
+    {
+        "name": "success-JPE",
+        "eos_data": [
+            {
+                "modelName": "DCS-7280SR3-48YC8",
+                "serialNumber": "JPE2130000",
+                "details": {
+                    "deviations": [],
+                    "components": [{"name": "FixedSystemvrm1", "version": "7"}],
+                },
+            }
+        ],
+        "side_effect": [],
+        "expected_result": "success",
+        "expected_messages": ["FN72 is mitigated"],
+    },
+    {
+        "name": "success-JAS",
+        "eos_data": [
+            {
+                "modelName": "DCS-7280SR3-48YC8",
+                "serialNumber": "JAS2040000",
+                "details": {
+                    "deviations": [],
+                    "components": [{"name": "FixedSystemvrm1", "version": "7"}],
+                },
+            }
+        ],
+        "side_effect": [],
+        "expected_result": "success",
+        "expected_messages": ["FN72 is mitigated"],
+    },
+    {
+        "name": "success-K-JPE",
+        "eos_data": [
+            {
+                "modelName": "DCS-7280SR3K-48YC8",
+                "serialNumber": "JPE2133000",
+                "details": {
+                    "deviations": [],
+                    "components": [{"name": "FixedSystemvrm1", "version": "7"}],
+                },
+            }
+        ],
+        "side_effect": [],
+        "expected_result": "success",
+        "expected_messages": ["FN72 is mitigated"],
+    },
+    {
+        "name": "success-K-JAS",
+        "eos_data": [
+            {
+                "modelName": "DCS-7280SR3K-48YC8",
+                "serialNumber": "JAS2040000",
+                "details": {
+                    "deviations": [],
+                    "components": [{"name": "FixedSystemvrm1", "version": "7"}],
+                },
+            }
+        ],
+        "side_effect": [],
+        "expected_result": "success",
+        "expected_messages": ["FN72 is mitigated"],
+    },
+    {
+        "name": "skipped-Serial",
+        "eos_data": [
+            {
+                "modelName": "DCS-7280SR3K-48YC8",
+                "serialNumber": "BAN2040000",
+                "details": {
+                    "deviations": [],
+                    "components": [{"name": "FixedSystemvrm1", "version": "7"}],
+                },
+            }
+        ],
+        "side_effect": [],
+        "expected_result": "skipped",
+        "expected_messages": ["Device not exposed"],
+    },
+    {
+        "name": "skipped-Platform",
+        "eos_data": [
+            {
+                "modelName": "DCS-7150-52-CL",
+                "serialNumber": "JAS0040000",
+                "details": {
+                    "deviations": [],
+                    "components": [{"name": "FixedSystemvrm1", "version": "5"}],
+                },
+            }
+        ],
+        "side_effect": [],
+        "expected_result": "skipped",
+        "expected_messages": ["Platform is not impacted by FN072"],
+    },
+    {
+        "name": "skipped-range-JPE",
+        "eos_data": [
+            {
+                "modelName": "DCS-7280SR3-48YC8",
+                "serialNumber": "JPE2131000",
+                "details": {
+                    "deviations": [],
+                    "components": [{"name": "FixedSystemvrm1", "version": "5"}],
+                },
+            }
+        ],
+        "side_effect": [],
+        "expected_result": "skipped",
+        "expected_messages": ["Device not exposed"],
+    },
+    {
+        "name": "skipped-range-K-JAS",
+        "eos_data": [
+            {
+                "modelName": "DCS-7280SR3K-48YC8",
+                "serialNumber": "JAS2041000",
+                "details": {
+                    "deviations": [],
+                    "components": [{"name": "FixedSystemvrm1", "version": "5"}],
+                },
+            }
+        ],
+        "side_effect": [],
+        "expected_result": "skipped",
+        "expected_messages": ["Device not exposed"],
+    },
+    {
+        "name": "failed-JPE",
+        "eos_data": [
+            {
+                "modelName": "DCS-7280SR3K-48YC8",
+                "serialNumber": "JPE2133000",
+                "details": {
+                    "deviations": [],
+                    "components": [{"name": "FixedSystemvrm1", "version": "5"}],
+                },
+            }
+        ],
+        "side_effect": [],
+        "expected_result": "failure",
+        "expected_messages": ["Device is exposed to FN72"],
+    },
+    {
+        "name": "failed-JAS",
+        "eos_data": [
+            {
+                "modelName": "DCS-7280SR3-48YC8",
+                "serialNumber": "JAS2040000",
+                "details": {
+                    "deviations": [],
+                    "components": [{"name": "FixedSystemvrm1", "version": "5"}],
+                },
+            }
+        ],
+        "side_effect": [],
+        "expected_result": "failure",
+        "expected_messages": ["Device is exposed to FN72"],
+    },
+    {
+        "name": "error",
+        "eos_data": [
+            {
+                "modelName": "DCS-7280SR3-48YC8",
+                "serialNumber": "JAS2040000",
+                "details": {
+                    "deviations": [],
+                    "components": [{"name": "FixedSystemvrm2", "version": "5"}],
+                },
+            }
+        ],
+        "side_effect": [],
+        "expected_result": "error",
+        "expected_messages": ["Error in running test - FixedSystemvrm1 not found"],
+    },
+]
--- tests/units/anta_tests/field_notices/test_exc.py
+++ tests/units/anta_tests/field_notices/test_exc.py
@@ -12,10 +12,10 @@ from unittest.mock import MagicMock
 
 import pytest
 
-from anta.tests.field_notices import VerifyFieldNotice44Resolution
+from anta.tests.field_notices import VerifyFieldNotice44Resolution, VerifyFieldNotice72Resolution
 from tests.lib.utils import generate_test_ids_list
 
-from .data import INPUT_FIELD_NOTICE_44_RESOLUTION
+from .data import INPUT_FIELD_NOTICE_44_RESOLUTION, INPUT_FIELD_NOTICE_72_RESOLUTION
 
 
 @pytest.mark.parametrize("test_data", INPUT_FIELD_NOTICE_44_RESOLUTION, ids=generate_test_ids_list(INPUT_FIELD_NOTICE_44_RESOLUTION))
@@ -26,6 +26,23 @@ def test_VerifyFieldNotice44Resolution(mocked_device: MagicMock, test_data: Any)
     logging.info(f"Mocked HW is: {mocked_device.hw_model}")
 
     test = VerifyFieldNotice44Resolution(mocked_device, eos_data=test_data["eos_data"])
+    asyncio.run(test.test())
+
+    logging.info(f"test result is: {test.result}")
+
+    assert str(test.result.name) == mocked_device.name
+    assert test.result.result == test_data["expected_result"]
+    assert test.result.messages == test_data["expected_messages"]
+
+
+@pytest.mark.parametrize("test_data", INPUT_FIELD_NOTICE_72_RESOLUTION, ids=generate_test_ids_list(INPUT_FIELD_NOTICE_72_RESOLUTION))
+def test_VerifyFieldNotice72Resolution(mocked_device: MagicMock, test_data: Any) -> None:
+    """Check VerifyFieldNotice72Resolution."""
+
+    logging.info(f"Mocked device is: {mocked_device.host}")
+    logging.info(f"Mocked HW is: {mocked_device.hw_model}")
+
+    test = VerifyFieldNotice72Resolution(mocked_device, eos_data=test_data["eos_data"])
     asyncio.run(test.test())
 
     logging.info(f"test result is: {test.result}")
