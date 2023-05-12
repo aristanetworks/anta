f06e21d60e6ba479982398748965838928799632
Colin MacGiollaEain <43743234+colinmacgiolla@users.noreply.github.com>
2023-05-12 16:20:58 +0100

    feat(anta.tests): Add field notices category (#195)

---
+++ docs/api/tests.field_notices.md
@@ -0,0 +1,4 @@
+# ANTA catalog for Field Notices tests
+
+::: anta.tests.field_notices
+    heading_level: 3
--- tests/units/anta_tests/software/test_exc.py
+++ tests/units/anta_tests/software/test_exc.py
@@ -12,10 +12,10 @@ from unittest.mock import MagicMock
 
 import pytest
 
-from anta.tests.software import VerifyEOSExtensions, VerifyEOSVersion, VerifyFieldNotice44Resolution, VerifyTerminAttrVersion
+from anta.tests.software import VerifyEOSExtensions, VerifyEOSVersion, VerifyTerminAttrVersion
 from tests.lib.utils import generate_test_ids_list
 
-from .data import INPUT_FIELD_NOTICE_44_RESOLUTION, INPUT_VERIFY_EOS_EXTENSIONS, INPUT_VERIFY_EOS_VERSION, INPUT_VERIFY_TERMINATTR_VERSION
+from .data import INPUT_VERIFY_EOS_EXTENSIONS, INPUT_VERIFY_EOS_VERSION, INPUT_VERIFY_TERMINATTR_VERSION
 
 
 @pytest.mark.parametrize("test_data", INPUT_VERIFY_EOS_VERSION, ids=generate_test_ids_list(INPUT_VERIFY_EOS_VERSION))
@@ -60,23 +60,6 @@ def test_VerifyEOSExtensions(mocked_device: MagicMock, test_data: Any) -> None:
     logging.info(f"Mocked HW is: {mocked_device.hw_model}")
 
     test = VerifyEOSExtensions(mocked_device, eos_data=test_data["eos_data"])
-    asyncio.run(test.test())
-
-    logging.info(f"test result is: {test.result}")
-
-    assert str(test.result.name) == mocked_device.name
-    assert test.result.result == test_data["expected_result"]
-    assert test.result.messages == test_data["expected_messages"]
-
-
-@pytest.mark.parametrize("test_data", INPUT_FIELD_NOTICE_44_RESOLUTION, ids=generate_test_ids_list(INPUT_FIELD_NOTICE_44_RESOLUTION))
-def test_VerifyFieldNotice44Resolution(mocked_device: MagicMock, test_data: Any) -> None:
-    """Check VerifyFieldNotice44Resolution."""
-
-    logging.info(f"Mocked device is: {mocked_device.host}")
-    logging.info(f"Mocked HW is: {mocked_device.hw_model}")
-
-    test = VerifyFieldNotice44Resolution(mocked_device, eos_data=test_data["eos_data"])
     asyncio.run(test.test())
 
     logging.info(f"test result is: {test.result}")
