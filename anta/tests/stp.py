1dffa49744fa29edc3125576cca9733d9c6cbf77
Carl Baillargeon <carl.baillargeon@arista.com>
2023-06-22 16:42:57 -0400

    Added STP tests + fix doc

---
+++ anta/tests/stp.py
@@ -0,0 +1,168 @@
+"""
+Test functions related to various Spanning Tree Protocol (STP) settings
+"""
+from __future__ import annotations
+
+import logging
+from typing import Any, Dict, cast
+
+from anta.models import AntaTest, AntaTestCommand, AntaTestTemplate
+
+logger = logging.getLogger(__name__)
+
+
+class VerifySTPMode(AntaTest):
+    """
+    Verifies the configured STP mode for a provided list of VLAN(s).
+
+    Expected Results:
+        * success: The test will pass if the STP mode is configured properly in the specified VLAN(s).
+        * failure: The test will fail if the STP mode is NOT configured properly for one or more specified VLAN(s).
+        * skipped: The test will be skipped if the STP mode is not provided.
+        * error: The test will give an error if a list of VLAN(s) is not provided as template_params.
+    """
+
+    name = "VerifySTPMode"
+    description = "Verifies the configured STP mode for a provided list of VLAN(s)."
+    categories = ["stp"]
+    template = AntaTestTemplate(template="show spanning-tree vlan {vlan}")
+
+    @staticmethod
+    def _check_stp_mode(mode: str) -> None:
+        """
+        Verifies if the provided STP mode is compatible with Arista EOS devices.
+
+        Args:
+            mode: The STP mode to verify.
+        """
+        stp_modes = ["mstp", "rstp", "rapidPvst"]
+
+        if mode not in stp_modes:
+            raise ValueError(f"Wrong STP mode provided. Valid modes are: {stp_modes}")
+
+    @AntaTest.anta_test
+    def test(self, mode: str = "mstp") -> None:
+        """
+        Run VerifySTPVersion validation.
+
+        Args:
+            mode: STP mode to verify. Defaults to 'mstp'.
+        """
+        if not mode:
+            self.result.is_skipped(f"{self.__class__.name} did not run because mode was not supplied")
+            return
+
+        self._check_stp_mode(mode)
+
+        logger.debug(f"self.instance_commands is: {self.instance_commands}")
+
+        self.result.is_success()
+
+        for index, command in enumerate(self.instance_commands):
+            vlan_id = cast(Dict[str, str], command.template_params).get("vlan")
+            command_output = cast(Dict[str, Dict[Any, Any]], self.instance_commands[index].output)
+            logger.debug(f"dataset for vlan {vlan_id} is: {command_output}")
+
+            if command_output["spanningTreeVlanInstances"][vlan_id]["spanningTreeVlanInstance"]["protocol"] != mode:
+                self.result.is_failure(f"Wrong STP mode configured for VLAN {vlan_id}")
+
+
+class VerifySTPBlockedPorts(AntaTest):
+    """
+    Verifies there is no STP blocked ports.
+
+    Expected Results:
+        * success: The test will pass if there are NO ports blocked by STP.
+        * failure: The test will fail if there are ports blocked by STP.
+    """
+
+    name = "VerifySTPBlockedPorts"
+    description = "Verifies there is no STP blocked ports."
+    categories = ["stp"]
+    commands = [AntaTestCommand(command="show spanning-tree blockedports")]
+
+    @AntaTest.anta_test
+    def test(self) -> None:
+        """
+        Run VerifySTPBlockedPorts validation
+        """
+        logger.debug(f"self.instance_commands is: {self.instance_commands}")
+
+        command_output = cast(Dict[str, Dict[Any, Any]], self.instance_commands[0].output)
+
+        if not (stp_instances := command_output["spanningTreeInstances"]):
+            self.result.is_success()
+        else:
+            for key, value in stp_instances.items():
+                stp_instances[key] = value.pop("spanningTreeBlockedPorts")
+            self.result.is_failure(f"The following ports are blocked by STP: {stp_instances}")
+
+
+class VerifySTPCounters(AntaTest):
+    """
+    Verifies there is no errors in STP BPDU packets.
+
+    Expected Results:
+        * success: The test will pass if there are NO STP BPDU packet errors under all interfaces participating in STP.
+        * failure: The test will fail if there are STP BPDU packet errors on one or more interface(s).
+    """
+
+    name = "VerifySTPCounters"
+    description = "Verifies there is no errors in STP BPDU packets."
+    categories = ["stp"]
+    commands = [AntaTestCommand(command="show spanning-tree counters")]
+
+    @AntaTest.anta_test
+    def test(self) -> None:
+        """Run VerifySTPBlockedPorts validation"""
+        logger.debug(f"self.instance_commands is: {self.instance_commands}")
+
+        command_output = cast(Dict[str, Dict[Any, Any]], self.instance_commands[0].output)
+
+        interfaces_with_errors = [
+            interface for interface, counters in command_output["interfaces"].items() if counters["bpduTaggedError"] or counters["bpduOtherError"] != 0
+        ]
+
+        if interfaces_with_errors:
+            self.result.is_failure(f"The following interfaces have STP BPDU packet errors: {interfaces_with_errors}")
+            return
+
+        self.result.is_success()
+
+
+class VerifySTPForwardingPorts(AntaTest):
+    """
+    Verifies that all interfaces are in a forwarding state for a provided list of VLAN(s).
+
+    Expected Results:
+        * success: The test will pass if all interfaces are in a forwarding state for the specified VLAN(s).
+        * failure: The test will fail if one of many interfaces are NOT in a forwarding state in one or more specified VLAN(s).
+        * error: The test will give an error if a list of VLAN(s) is not provided as template_params.
+    """
+
+    name = "VerifySTPForwardingPorts"
+    description = "Verifies that all interfaces are forwarding for a provided list of VLAN(s)."
+    categories = ["stp"]
+    template = AntaTestTemplate(template="show spanning-tree topology vlan {vlan} status")
+
+    @AntaTest.anta_test
+    def test(self) -> None:
+        """
+        Run VerifySTPForwardingPorts validation.
+        """
+
+        logger.debug(f"self.instance_commands is: {self.instance_commands}")
+
+        self.result.is_success()
+
+        for index, command in enumerate(self.instance_commands):
+            vlan_id = cast(Dict[str, str], command.template_params).get("vlan")
+            command_output = cast(Dict[str, Dict[Any, Any]], self.instance_commands[index].output)
+            logger.debug(f"dataset for vlan {vlan_id} is: {command_output}")
+
+            for value in command_output["topologies"].values():
+                if int(vlan_id) in value["vlans"]:
+                    interfaces_not_forwarding = [interface for interface, state in value["interfaces"].items() if state["state"] != "forwarding"]
+
+            if interfaces_not_forwarding:
+                self.result.is_failure(f"The following interface(s) are not in a forwarding state for VLAN {vlan_id}: {interfaces_not_forwarding}")
--- docs/api/tests.md
+++ docs/api/tests.md
@@ -3,16 +3,24 @@
 This section describes all the available tests provided by ANTA package.
 
 
+- [AAA](tests.aaa.md)
 - [Configuration](tests.configuration.md)
+- [Field Notice](tests.field_notices.md)
 - [Hardware](tests.hardware.md)
-- [interfaces](tests.interfaces.md)
+- [Interfaces](tests.interfaces.md)
+- [Logging](tests.logging.md)
 - [MLAG](tests.mlag.md)
 - [Multicast](tests.multicast.md)
 - [Profiles](tests.profiles.md)
-- [System](tests.system.md)
-- [Software](tests.software.md)
 - [Routing Generic](tests.routing.generic.md)
 - [Routing BGP](tests.routing.bgp.md)
 - [Routing OSPF](tests.routing.ospf.md)
+- [Security](tests.security.md)
+- [SNMP](tests.snmp.md)
+- [Software](tests.software.md)
+- [STP](tests.stp.md)
+- [System](tests.system.md)
+
+
 
 All these tests can be imported in a [catalog](../usage-inventory-catalog.md) to be used by [the anta cli](../cli/nrfu.md) or in your [own framework](../advanced_usages/as-python-lib.md)
