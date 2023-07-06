40865d2b5b511018a1b5e35c220bcac7df11befc
carl-baillargeon <63206086+carl-baillargeon@users.noreply.github.com>
2023-07-06 08:56:55 -0400

    feat(anta.tests): MLAG tests update (#246)
    * Update MLAG tests

--- tests/units/anta_tests/mlag/test_exc.py
+++ tests/units/anta_tests/mlag/test_exc.py
@@ -10,10 +10,10 @@ from unittest.mock import MagicMock
 
 import pytest
 
-from anta.tests.mlag import VerifyMlagConfigSanity, VerifyMlagInterfaces, VerifyMlagStatus
+from anta.tests.mlag import VerifyMlagConfigSanity, VerifyMlagDualPrimary, VerifyMlagInterfaces, VerifyMlagReloadDelay, VerifyMlagStatus
 from tests.lib.utils import generate_test_ids_list
 
-from .data import INPUT_MLAG_CONFIG_SANITY, INPUT_MLAG_INTERFACES, INPUT_MLAG_STATUS
+from .data import INPUT_MLAG_CONFIG_SANITY, INPUT_MLAG_DUAL_PRIMARY, INPUT_MLAG_INTERFACES, INPUT_MLAG_RELOAD_DELAY, INPUT_MLAG_STATUS
 
 
 @pytest.mark.parametrize("test_data", INPUT_MLAG_STATUS, ids=generate_test_ids_list(INPUT_MLAG_STATUS))
@@ -57,6 +57,45 @@ def test_VerifyMlagConfigSanity(mocked_device: MagicMock, test_data: Any) -> Non
 
     test = VerifyMlagConfigSanity(mocked_device, eos_data=test_data["eos_data"])
     asyncio.run(test.test())
+    logging.info(f"test result is: {test.result}")
+
+    assert str(test.result.name) == mocked_device.name
+    assert test.result.result == test_data["expected_result"]
+    assert test.result.messages == test_data["expected_messages"]
+
+
+@pytest.mark.parametrize("test_data", INPUT_MLAG_RELOAD_DELAY, ids=generate_test_ids_list(INPUT_MLAG_RELOAD_DELAY))
+def test_VerifyMlagReloadDelay(mocked_device: MagicMock, test_data: Any) -> None:
+    """Check VerifyMlagReloadDelay"""
+
+    logging.info(f"Mocked device is: {mocked_device.host}")
+    logging.info(f"Mocked HW is: {mocked_device.hw_model}")
+
+    test = VerifyMlagReloadDelay(mocked_device, eos_data=test_data["eos_data"])
+    asyncio.run(test.test(reload_delay=test_data["side_effect"]["reload_delay"], reload_delay_non_mlag=test_data["side_effect"]["reload_delay_non_mlag"]))
+    logging.info(f"test result is: {test.result}")
+
+    assert str(test.result.name) == mocked_device.name
+    assert test.result.result == test_data["expected_result"]
+    assert test.result.messages == test_data["expected_messages"]
+
+
+@pytest.mark.parametrize("test_data", INPUT_MLAG_DUAL_PRIMARY, ids=generate_test_ids_list(INPUT_MLAG_DUAL_PRIMARY))
+def test_VerifyMlagDualPrimary(mocked_device: MagicMock, test_data: Any) -> None:
+    """Check VerifyMlagDualPrimary"""
+
+    logging.info(f"Mocked device is: {mocked_device.host}")
+    logging.info(f"Mocked HW is: {mocked_device.hw_model}")
+
+    test = VerifyMlagDualPrimary(mocked_device, eos_data=test_data["eos_data"])
+    asyncio.run(
+        test.test(
+            detection_delay=test_data["side_effect"]["detection_delay"],
+            errdisabled=test_data["side_effect"]["errdisabled"],
+            recovery_delay=test_data["side_effect"]["recovery_delay"],
+            recovery_delay_non_mlag=test_data["side_effect"]["recovery_delay_non_mlag"],
+        )
+    )
     logging.info(f"test result is: {test.result}")
 
     assert str(test.result.name) == mocked_device.name
