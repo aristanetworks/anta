f33ba050b17ee7ff143daa4a9fe63e3434a134e8
Matthieu Tâche <tache.matthieu@gmail.com>
2023-08-11 14:53:35 +0200

    anta.tests.profiles: refactor tests

---
+++ tests/units/anta_tests/test_profiles.py
@@ -0,0 +1,44 @@
+"""
+Tests for anta.tests.profiles.py
+"""
+from __future__ import annotations
+
+from typing import Any
+
+from anta.tests.profiles import VerifyTcamProfile, VerifyUnifiedForwardingTableMode
+from tests.units.anta_tests.test_case import test
+
+DATA: list[dict[str, Any]] = [
+    {
+        "name": "success",
+        "test": VerifyUnifiedForwardingTableMode,
+        "eos_data": [{"uftMode": "2", "urpfEnabled": False, "chipModel": "bcm56870", "l2TableSize": 163840, "l3TableSize": 147456, "lpmTableSize": 32768}],
+        "inputs": {"mode": 2},
+        "expected": {"result": "success"},
+    },
+    {
+        "name": "failure",
+        "test": VerifyUnifiedForwardingTableMode,
+        "eos_data": [{"uftMode": "2", "urpfEnabled": False, "chipModel": "bcm56870", "l2TableSize": 163840, "l3TableSize": 147456, "lpmTableSize": 32768}],
+        "inputs": {"mode": 3},
+        "expected": {"result": "failure", "messages": ["Device is not running correct UFT mode (expected: 3 / running: 2)"]},
+    },
+    {
+        "name": "success",
+        "test": VerifyTcamProfile,
+        "eos_data": [
+            {"pmfProfiles": {"FixedSystem": {"config": "test", "configType": "System Profile", "status": "test", "mode": "tcam"}}, "lastProgrammingStatus": {}}
+        ],
+        "inputs": {"profile": "test"},
+        "expected": {"result": "success"},
+    },
+    {
+        "name": "failure",
+        "test": VerifyTcamProfile,
+        "eos_data": [
+            {"pmfProfiles": {"FixedSystem": {"config": "test", "configType": "System Profile", "status": "default", "mode": "tcam"}}, "lastProgrammingStatus": {}}
+        ],
+        "inputs": {"profile": "test"},
+        "expected": {"result": "failure", "messages": ["Incorrect profile running on device: default"]},
+    },
+]
