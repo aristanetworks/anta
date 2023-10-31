a4301d44e2ecaa4c6515efa91fcc414490efd37e
gmuloc <gmulocher@arista.com>
2023-10-31 15:39:00 +0100

    Test: Fix result_manager tests to not tshoot the CI

--- tests/units/result_manager/test_models.py
+++ tests/units/result_manager/test_models.py
@@ -4,197 +4,54 @@
 """ANTA Result Manager models unit tests."""
 from __future__ import annotations
 
-import logging
-from typing import Any
+from typing import Any, Callable
 
 import pytest
 
 from anta.result_manager.models import TestResult
-from tests.data.json_data import TEST_RESULT_UNIT
+from tests.data.json_data import TEST_RESULT_SET_STATUS
 from tests.lib.utils import generate_test_ids_dict
 
-pytest.skip(reason="Not yet ready for CI", allow_module_level=True)
+# pytest.skip(reason="Not yet ready for CI", allow_module_level=True)
 
 
-class Test_InventoryUnitModels:
+class TestTestResultModels:
     """Test components of anta.result_manager.models."""
 
-    @pytest.mark.parametrize("test_definition", TEST_RESULT_UNIT, ids=generate_test_ids_dict)
-    def test_anta_result_init_valid(self, test_definition: dict[str, Any]) -> None:
-        """Test Result model.
+    @pytest.mark.parametrize("data", TEST_RESULT_SET_STATUS, ids=generate_test_ids_dict)
+    def test__is_status_foo(self, test_result_factory: Callable[[int], TestResult], data: dict[str, Any]) -> None:
+        """Test TestResult.is_foo methods."""
+        testresult = test_result_factory(1)
+        assert testresult.result == "unset"
+        assert len(testresult.messages) == 0
+        if data["target"] == "success":
+            testresult.is_success(data["message"])
+            assert testresult.result == data["target"]
+            assert data["message"] in testresult.messages
+        if data["target"] == "failure":
+            testresult.is_failure(data["message"])
+            assert testresult.result == data["target"]
+            assert data["message"] in testresult.messages
+        if data["target"] == "error":
+            testresult.is_error(data["message"])
+            assert testresult.result == data["target"]
+            assert data["message"] in testresult.messages
+        if data["target"] == "skipped":
+            testresult.is_skipped(data["message"])
+            assert testresult.result == data["target"]
+            assert data["message"] in testresult.messages
+        # no helper for unset, testing _set_status
+        if data["target"] == "unset":
+            testresult._set_status("unset", data["message"])  # pylint: disable=W0212
+            assert testresult.result == data["target"]
+            assert data["message"] in testresult.messages
 
-        Test structure:
-        ---------------
-            {
-                'name': 'valid_full',
-                'input': {"host": '1.1.1.1', 'test': 'pytest_unit_test', 'result': 'success', 'messages': ['test']},
-                'expected_result': 'valid',
-            }
-
-        """
-        if test_definition["expected_result"] == "invalid":
-            pytest.skip("Not concerned by the test")
-        try:
-            result = TestResult(**test_definition["input"])
-            logging.info(f"TestResult is {result.dict()}")
-        # pylint: disable=W0703
-        except Exception as e:
-            logging.error(f"Error loading data:\n{str(e)}")
-            assert False
-
-    @pytest.mark.parametrize("test_definition", TEST_RESULT_UNIT, ids=generate_test_ids_dict)
-    def test_anta_result_init_invalid(self, test_definition: dict[str, Any]) -> None:
-        """Test Result model.
-
-        Test structure:
-        ---------------
-            {
-                'name': 'valid_full',
-                'input': {"host": '1.1.1.1', 'test': 'pytest_unit_test', 'result': 'success', 'messages': ['test']},
-                'expected_result': 'valid',
-            }
-
-        """
-        if test_definition["expected_result"] == "valid":
-            pytest.skip("Not concerned by the test")
-        try:
-            TestResult(**test_definition["input"])
-        except ValueError as e:
-            logging.warning(f"Error loading data:\n{str(e)}")
-        else:
-            logging.error("An exception is expected here")
-            assert False
-
-    @pytest.mark.parametrize("test_definition", TEST_RESULT_UNIT, ids=generate_test_ids_dict)
-    def test_anta_result_set_status_success(self, test_definition: dict[str, Any]) -> None:
-        """Test Result model.
-
-        Test structure:
-        ---------------
-            {
-                'name': 'valid_full',
-                'input': {"host": '1.1.1.1', 'test': 'pytest_unit_test', 'result': 'success', 'messages': ['test']},
-                'expected_result': 'valid',
-            }
-
-        """
-        if test_definition["expected_result"] == "invalid":
-            pytest.skip("Not concerned by the test")
-
-        result = TestResult(**test_definition["input"])
-
-        result.is_success()
-        assert result.result == "success"
-        result_message_len = len(result.messages)
-
-        if "messages" in test_definition["input"]:
-            assert result_message_len == len(test_definition["input"]["messages"])
-        else:
-            assert result_message_len == 0
-
-        # Adding one message
-        result.is_success("it is a great success")
-        assert result.result == "success"
-        assert len(result.messages) == result_message_len + 1
-
-    @pytest.mark.parametrize("test_definition", TEST_RESULT_UNIT, ids=generate_test_ids_dict)
-    def test_anta_result_set_status_failure(self, test_definition: dict[str, Any]) -> None:
-        """Test Result model.
-
-        Test structure:
-        ---------------
-            {
-                'name': 'valid_full',
-                'input': {"host": '1.1.1.1', 'test': 'pytest_unit_test', 'result': 'success', 'messages': ['test']},
-                'expected_result': 'valid',
-            }
-
-        """
-        if test_definition["expected_result"] == "invalid":
-            pytest.skip("Not concerned by the test")
-
-        result = TestResult(**test_definition["input"])
-
-        result.is_failure()
-        assert result.result == "failure"
-        result_message_len = len(result.messages)
-
-        if "messages" in test_definition["input"]:
-            assert result_message_len == len(test_definition["input"]["messages"])
-        else:
-            assert result_message_len == 0
-
-        # Adding one message
-        result.is_failure("it is a great failure")
-        assert result.result == "failure"
-        assert len(result.messages) == result_message_len + 1
-
-    @pytest.mark.parametrize("test_definition", TEST_RESULT_UNIT, ids=generate_test_ids_dict)
-    def test_anta_result_set_status_error(self, test_definition: dict[str, Any]) -> None:
-        """Test Result model.
-
-        Test structure:
-        ---------------
-            {
-                'name': 'valid_full',
-                'input': {"host": '1.1.1.1', 'test': 'pytest_unit_test', 'result': 'success', 'messages': ['test']},
-                'expected_result': 'valid',
-            }
-
-        """
-        if test_definition["expected_result"] == "invalid":
-            pytest.skip("Not concerned by the test")
-
-        result = TestResult(**test_definition["input"])
-
-        result.is_error()
-        assert result.result == "error"
-        result_message_len = len(result.messages)
-
-        if "messages" in test_definition["input"]:
-            assert result_message_len == len(test_definition["input"]["messages"])
-        else:
-            assert result_message_len == 0
-
-        # Adding one message
-        result.is_error(message="it is a great error")
-        assert result.result == "error"
-        assert len(result.messages) == result_message_len + 1
-
-        # Adding one exception object
-        e = Exception()
-        result.is_error(exception=e)
-        assert result.result == "error"
-        assert result.error == e
-
-    @pytest.mark.parametrize("test_definition", TEST_RESULT_UNIT, ids=generate_test_ids_dict)
-    def test_anta_result_set_status_skipped(self, test_definition: dict[str, Any]) -> None:
-        """Test Result model.
-
-        Test structure:
-        ---------------
-            {
-                'name': 'valid_full',
-                'input': {"host": '1.1.1.1', 'test': 'pytest_unit_test', 'result': 'success', 'messages': ['test']},
-                'expected_result': 'valid',
-            }
-
-        """
-        if test_definition["expected_result"] == "invalid":
-            pytest.skip("Not concerned by the test")
-
-        result = TestResult(**test_definition["input"])
-
-        result.is_skipped()
-        assert result.result == "skipped"
-        result_message_len = len(result.messages)
-
-        if "messages" in test_definition["input"]:
-            assert result_message_len == len(test_definition["input"]["messages"])
-        else:
-            assert result_message_len == 0
-
-        # Adding one message
-        result.is_skipped("it is a great skipped")
-        assert result.result == "skipped"
-        assert len(result.messages) == result_message_len + 1
+    @pytest.mark.parametrize("data", TEST_RESULT_SET_STATUS, ids=generate_test_ids_dict)
+    def test____str__(self, test_result_factory: Callable[[int], TestResult], data: dict[str, Any]) -> None:
+        """Test TestResult.__str__."""
+        testresult = test_result_factory(1)
+        assert testresult.result == "unset"
+        assert len(testresult.messages) == 0
+        testresult._set_status(data["target"], data["message"])  # pylint: disable=W0212
+        assert testresult.result == data["target"]
+        assert str(testresult) == f"Test VerifyTest1 on device testdevice has result {data['target']}"
