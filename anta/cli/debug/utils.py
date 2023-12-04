2de4bcab5326c273585e2003d294ec2af10597dd
Matthieu Tâche <tache.matthieu@gmail.com>
2023-12-04 15:06:38 +0100

    Update anta/cli/debug/utils.py
    Co-authored-by: Guillaume Mulocher <gmulocher@arista.com>

--- anta/cli/debug/utils.py
+++ anta/cli/debug/utils.py
@@ -2,7 +2,6 @@
 # Copyright (c) 2023 Arista Networks, Inc.
 # Use of this source code is governed by the Apache License 2.0
 # that can be found in the LICENSE file.
-# coding: utf-8 -*-
 """
 Utils functions to use with anta.cli.debug module.
 """
