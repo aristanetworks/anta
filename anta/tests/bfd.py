2e4ab4472da9eb41c6e11ba2da1b7f84c4fc0da1
Mahesh <mahesh.kumar-ext@arista.com>
2023-12-20 08:50:34 +0000

    issue-499: Added testcase for BFD session and timers

---
+++ anta/tests/bfd.py
@@ -0,0 +1,124 @@
+# Copyright (c) 2023 Arista Networks, Inc.
+# Use of this source code is governed by the Apache License 2.0
+# that can be found in the LICENSE file.
+"""
+BFD test functions
+"""
+# Mypy does not understand AntaTest.Input typing
+# mypy: disable-error-code=attr-defined
+from __future__ import annotations
+
+from ipaddress import IPv4Address, IPv6Address, ip_address
+from typing import Any, List, Union, cast
+
+from pydantic import BaseModel
+
+from anta.models import AntaCommand, AntaTemplate, AntaTest
+from anta.tools.get_value import get_value
+
+
+class VerifyBFDPeers(AntaTest):
+    """
+    This class verifies if the BFD (Bidirectional Forwarding Detection) neighbor's sessions are UP and
+    if the timers are correctly set in the specified VRF (Virtual Routing and Forwarding).
+
+    Expected results:
+        * success: The test will pass if BFD neighbors are UP and timers are correctly set in the specified VRF.
+        * failure: The test will fail if BFD neighbors are not found, status is not UP, or timers are not as expected in the specified VRF.
+    """
+
+    name = "VerifyBFDPeers"
+    description = "Verifies if BFD neighbor's sessions are UP and timers are correctly set in the specified VRF."
+    categories = ["bfd"]
+    commands = [AntaTemplate(template="show bfd peers dest-ip {neighbor} vrf {vrf} detail")]
+
+    class Input(AntaTest.Input):
+        """
+        This class defines the input parameters of the testcase.
+        """
+
+        bfd_neighbors: List["BFDNeighbors"]
+        """List of BFD neighbors"""
+
+        class BFDNeighbors(BaseModel):
+            """
+            This class defines the details of a BFD neighbor.
+            """
+
+            neighbor: Union[IPv4Address, IPv6Address]
+            """IPv4/IPv6 BFD neighbor"""
+            vrf: str = "default"
+            """VRF context"""
+            loopback: Union[IPv4Address, IPv6Address]
+            """Loopback IP address"""
+            tx_interval: int
+            """Tx interval of BFD neighbor"""
+            rx_interval: int
+            """Rx interval of BFD neighbor"""
+            multiplier: int
+            """Multiplier of BFD neighbor"""
+
+    def render(self, template: AntaTemplate) -> list[AntaCommand]:
+        """
+        This method renders the template with the BFD neighbor details.
+        """
+        return [
+            template.render(
+                neighbor=bfd_neighbor.neighbor,
+                vrf=bfd_neighbor.vrf,
+                loopback=bfd_neighbor.loopback,
+                tx_interval=bfd_neighbor.tx_interval,
+                rx_interval=bfd_neighbor.rx_interval,
+                multiplier=bfd_neighbor.multiplier,
+            )
+            for bfd_neighbor in self.inputs.bfd_neighbors
+        ]
+
+    @AntaTest.anta_test
+    def test(self) -> None:
+        """
+        This method tests the BFD neighbors.
+        """
+        failures: dict[str, Any] = {}
+
+        # Iterating over command output for different neighbors
+        for command in self.instance_commands:
+            neighbor = cast(str, command.params.get("neighbor"))
+            vrf = cast(str, command.params.get("vrf"))
+            loopback = command.params.get("loopback")
+            tx_interval = command.params.get("tx_interval")
+            rx_interval = command.params.get("rx_interval")
+            multiplier = command.params.get("multiplier")
+
+            # Verify BFD neighbor state and timers
+            if not (
+                bfd_output := get_value(
+                    command.json_output,
+                    f"vrfs..{vrf}..{'ipv4Neighbors' if isinstance(ip_address(neighbor), IPv4Address) else 'ipv6Neighbors'}..{neighbor}"
+                    f"..peers....types..multihop..peerStats..{loopback}",
+                    separator="..",
+                )
+            ):
+                failures[str(neighbor)] = {vrf: "Not Configured"}
+                continue
+
+            bfd_details = bfd_output["peerStatsDetail"]
+            if (
+                bfd_output["status"] != "up"
+                or bfd_details["operTxInterval"] != tx_interval
+                or bfd_details["operRxInterval"] != rx_interval
+                or bfd_details["detectMult"] != multiplier
+            ):
+                failures[str(neighbor)] = {
+                    vrf: {
+                        "status": bfd_output["status"],
+                        "tx_interval": bfd_details["operTxInterval"],
+                        "rx_interval": bfd_details["operRxInterval"],
+                        "multiplier": bfd_details["detectMult"],
+                    }
+                }
+
+        if not failures:
+            self.result.is_success()
+        else:
+            self.result.is_failure(f"Following BFD neighbors are not UP, not configured or timers are not ok:\n{failures}")
---
+++ tests/units/anta_tests/test_bfd.py
@@ -0,0 +1,176 @@
+# Copyright (c) 2023 Arista Networks, Inc.
+# Use of this source code is governed by the Apache License 2.0
+# that can be found in the LICENSE file.
+"""
+Tests for anta.tests.bfd.py
+"""
+# pylint: disable=C0302
+from __future__ import annotations
+
+from typing import Any
+
+# pylint: disable=C0413
+# because of the patch above
+from anta.tests.bfd import VerifyBFDPeers  # noqa: E402
+from tests.lib.anta import test  # noqa: F401; pylint: disable=W0611
+
+DATA: list[dict[str, Any]] = [
+    {
+        "name": "success",
+        "test": VerifyBFDPeers,
+        "eos_data": [
+            {
+                "vrfs": {
+                    "default": {
+                        "ipv4Neighbors": {
+                            "192.0.255.7": {
+                                "peers": {
+                                    "": {
+                                        "types": {
+                                            "multihop": {
+                                                "peerStats": {
+                                                    "192.0.255.1": {
+                                                        "status": "up",
+                                                        "peerStatsDetail": {
+                                                            "operTxInterval": 1200000,
+                                                            "operRxInterval": 1200000,
+                                                            "detectMult": 3,
+                                                        },
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        ],
+        "inputs": {
+            "bfd_neighbors": [
+                {"neighbor": "192.0.255.7", "vrf": "default", "loopback": "192.0.255.1", "tx_interval": 1200000, "rx_interval": 1200000, "multiplier": 3}
+            ]
+        },
+        "expected": {"result": "success"},
+    },
+    {
+        "name": "failure-no-vrf",
+        "test": VerifyBFDPeers,
+        "eos_data": [{"vrfs": {}, "errors": ["VRF MGMT not configured."]}],
+        "inputs": {
+            "bfd_neighbors": [{"neighbor": "192.0.255.7", "vrf": "MGMT", "loopback": "192.0.255.1", "tx_interval": 1200000, "rx_interval": 1200000, "multiplier": 3}]
+        },
+        "expected": {
+            "result": "failure",
+            "messages": ["Following BFD neighbors are not UP, not configured or timers are not ok:\n{'192.0.255.7': {'MGMT': 'Not Configured'}}"],
+        },
+    },
+    {
+        "name": "failure-no-neighbor",
+        "test": VerifyBFDPeers,
+        "eos_data": [{"vrfs": {}}],
+        "inputs": {
+            "bfd_neighbors": [
+                {"neighbor": "192.0.255.70", "vrf": "default", "loopback": "192.0.255.1", "tx_interval": 1200000, "rx_interval": 1200000, "multiplier": 3}
+            ]
+        },
+        "expected": {
+            "result": "failure",
+            "messages": ["Following BFD neighbors are not UP, not configured or timers are not ok:\n{'192.0.255.70': {'default': 'Not Configured'}}"],
+        },
+    },
+    {
+        "name": "failure-session-down",
+        "test": VerifyBFDPeers,
+        "eos_data": [
+            {
+                "vrfs": {
+                    "default": {
+                        "ipv4Neighbors": {
+                            "192.0.255.7": {
+                                "peers": {
+                                    "": {
+                                        "types": {
+                                            "multihop": {
+                                                "peerStats": {
+                                                    "192.0.255.1": {
+                                                        "status": "down",
+                                                        "peerStatsDetail": {
+                                                            "operTxInterval": 1200000,
+                                                            "operRxInterval": 1200000,
+                                                            "detectMult": 3,
+                                                        },
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        ],
+        "inputs": {
+            "bfd_neighbors": [
+                {"neighbor": "192.0.255.7", "vrf": "default", "loopback": "192.0.255.1", "tx_interval": 1200000, "rx_interval": 1200000, "multiplier": 3}
+            ]
+        },
+        "expected": {
+            "result": "failure",
+            "messages": [
+                "Following BFD neighbors are not UP, not configured or timers are not ok:\n"
+                "{'192.0.255.7': {'default': {'status': 'down', 'tx_interval': 1200000, 'rx_interval': 1200000, 'multiplier': 3}}}"
+            ],
+        },
+    },
+    {
+        "name": "failure-incorrect-timers",
+        "test": VerifyBFDPeers,
+        "eos_data": [
+            {
+                "vrfs": {
+                    "default": {
+                        "ipv4Neighbors": {
+                            "192.0.255.7": {
+                                "peers": {
+                                    "": {
+                                        "types": {
+                                            "multihop": {
+                                                "peerStats": {
+                                                    "192.0.255.1": {
+                                                        "status": "up",
+                                                        "peerStatsDetail": {
+                                                            "operTxInterval": 1300000,
+                                                            "operRxInterval": 1300000,
+                                                            "detectMult": 4,
+                                                        },
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        ],
+        "inputs": {
+            "bfd_neighbors": [
+                {"neighbor": "192.0.255.7", "vrf": "default", "loopback": "192.0.255.1", "tx_interval": 1200000, "rx_interval": 1200000, "multiplier": 3}
+            ]
+        },
+        "expected": {
+            "result": "failure",
+            "messages": [
+                "Following BFD neighbors are not UP, not configured or timers are not ok:\n"
+                "{'192.0.255.7': {'default': {'status': 'up', 'tx_interval': 1300000, 'rx_interval': 1300000, 'multiplier': 4}}}"
+            ],
+        },
+    },
+]
