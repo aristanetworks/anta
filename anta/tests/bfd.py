1be46af786b77405c17f59ded610d9d461b23dea
Mahesh <mahesh.kumar-ext@arista.com>
2023-12-20 12:45:13 +0000

    issue-499: removed model_rebuild method

--- anta/tests/bfd.py
+++ anta/tests/bfd.py
@@ -58,8 +58,6 @@ class VerifyBFDPeers(AntaTest):
             multiplier: int
             """Multiplier of BFD neighbor"""
 
-    Input.model_rebuild()
-
     def render(self, template: AntaTemplate) -> List[AntaCommand]:
         """
         This method renders the template with the BFD neighbor details.
