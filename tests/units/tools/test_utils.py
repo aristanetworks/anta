473ebfa137854f433157fa879435480381278213
Mahesh Kumar <mahesh.kumar-ext@arista.com>
2024-01-15 06:31:13 +0000

    issue-520: fixed the formatting issue

--- tests/units/tools/test_utils.py
+++ tests/units/tools/test_utils.py
@@ -31,8 +31,23 @@ ACTUAL_OUTPUTS = [
     "expected_output, actual_output, expected_result, expected_raise",
     [
         pytest.param(EXPECTED_OUTPUTS[0], ACTUAL_OUTPUTS[0], "", does_not_raise(), id="no difference"),
-        pytest.param(EXPECTED_OUTPUTS[0], ACTUAL_OUTPUTS[1], "\nExpected `1` as the id, but found `2` instead.\nExpected `Alice` as the name, but found `Bob` instead.\nExpected `30` as the age, but found `35` instead.\nExpected `alice@example.com` as the email, but found `bob@example.com` instead.", does_not_raise(), id="different data"),
-        pytest.param(EXPECTED_OUTPUTS[0], {}, "\nExpected `1` as the id, but it was not found in the actual output.\nExpected `Alice` as the name, but it was not found in the actual output.\nExpected `30` as the age, but it was not found in the actual output.\nExpected `alice@example.com` as the email, but it was not found in the actual output.", does_not_raise(), id="empty actual output"),
+        pytest.param(
+            EXPECTED_OUTPUTS[0],
+            ACTUAL_OUTPUTS[1],
+            "\nExpected `1` as the id, but found `2` instead.\nExpected `Alice` as the name, but found `Bob` instead.\n"
+            "Expected `30` as the age, but found `35` instead.\nExpected `alice@example.com` as the email, but found `bob@example.com` instead.",
+            does_not_raise(),
+            id="different data",
+        ),
+        pytest.param(
+            EXPECTED_OUTPUTS[0],
+            {},
+            "\nExpected `1` as the id, but it was not found in the actual output.\nExpected `Alice` as the name, but it was not found in the actual output.\n"
+            "Expected `30` as the age, but it was not found in the actual output.\nExpected `alice@example.com` as the email, but it was not found in "
+            "the actual output.",
+            does_not_raise(),
+            id="empty actual output",
+        ),
         pytest.param(EXPECTED_OUTPUTS[3], ACTUAL_OUTPUTS[3], "\nExpected `Jon` as the name, but found `Rob` instead.", does_not_raise(), id="different name"),
     ],
 )
