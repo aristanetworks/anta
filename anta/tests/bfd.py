ecceab1f5f9e7b5f542ece8f47648150b07ebc13
Mahesh Kumar <mahesh.kumar-ext@arista.com>
2024-01-15 09:40:54 +0000

    issue-503: Removed IPv6 support for now

--- anta/tests/bfd.py
+++ anta/tests/bfd.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023 Arista Networks, Inc.
+# Copyright (c) 2023-2024 Arista Networks, Inc.
 # Use of this source code is governed by the Apache License 2.0
 # that can be found in the LICENSE file.
 """
@@ -16,14 +16,14 @@ from anta.models import AntaCommand, AntaTest
 
 class VerifyBFDPeersHealth(AntaTest):
     """
-    Verifies there is no BFD peer in the down state, remote disc is not zero and last down should be above the threshold (all VRF, IPv4/ IPv6 peers).
+    Verifies there is no IPv4 BFD peer in the down state, remote disc is not zero and last down should be above the threshold for all VRF.
     Expected results:
-        * success: The test will pass if BFD peers are not down, remote disc is not zero and last down above the defined threshold (all VRF, IPv4/ IPv6 peers).
-        * failure: The test will fail if BFD peers are down, remote disc is zero and last down below the defined threshold (all VRF, IPv4/ IPv6 peers).
+        * success: The test will pass if IPv4 BFD peers are not down, remote disc is not zero and last down above the defined threshold for all VRF.
+        * failure: The test will fail if IPv4 BFD peers are down, remote disc is zero and last down below the defined threshold for all VRF.
     """
 
     name = "VerifyBFDPeersHealth"
-    description = "Verifies there is no BFD peer in the down state, remote disc is not zero and last down should be above the threshold (all VRF, IPv4/IPv6 peers)."
+    description = "Verifies there is no IPv4 BFD peer in the down state, remote disc is not zero and last down should be above the threshold for all VRF."
     categories = ["bfd"]
 
     # revision 1 as later revision introduces additional nesting for type
@@ -48,32 +48,30 @@ class VerifyBFDPeersHealth(AntaTest):
         current_timestamp = clock_output["utcTime"]
         bfd_output = self.instance_commands[0].json_output
 
-        # Check the initial result
+        # set the initial result
         self.result.is_success()
 
-        # Iterate over IPv4 and IPv6 neighbors
-        for ip_type in ["ipv4Neighbors", "ipv6Neighbors"]:
-            for vrf, vrf_data in bfd_output["vrfs"].items():
-                for peer, neighbor_data in vrf_data[ip_type].items():
-                    for peer_data in neighbor_data["peerStats"].values():
-                        peer_status = peer_data["status"]
-                        remote_disc = peer_data["remoteDisc"]
-                        peer_l3intf = peer_data.get("l3intf", "")
-                        l3intf_info = f" with peer layer3 interface {peer_l3intf}" if peer_l3intf else ""
-                        remote_disc_info = f" with remote disc {remote_disc}" if remote_disc == 0 else ""
-                        remote_disc_info = f" and remote disc {remote_disc}" if remote_disc == 0 and peer_l3intf else remote_disc_info
-                        last_down = peer_data["lastDown"]
-                        hours_difference = (datetime.fromtimestamp(current_timestamp) - datetime.fromtimestamp(last_down)).total_seconds() / 3600
-
-                        # Check if peer status is not up
-                        if peer_status != "up":
-                            down_failures.append(f"{peer} is {peer_status} in {vrf} VRF{l3intf_info}{remote_disc_info}.")
-                        # Check if the last down is within the threshold
-                        elif self.inputs.last_down and hours_difference > self.inputs.last_down:
-                            up_failures.append(f"{peer} in {vrf} VRF was down {round(hours_difference)} hours ago{l3intf_info}{remote_disc_info}.")
-                        # Check if remote disc is 0
-                        elif remote_disc == 0:
-                            up_failures.append(f"{peer} in {vrf} VRF has remote disc {remote_disc}{l3intf_info}.")
+        # Iterate over IPv4 BFD peers
+        for vrf, vrf_data in bfd_output["vrfs"].items():
+            for peer, neighbor_data in vrf_data["ipv4Neighbors"].items():
+                for peer_data in neighbor_data["peerStats"].values():
+                    peer_status = peer_data["status"]
+                    remote_disc = peer_data["remoteDisc"]
+                    peer_l3intf = peer_data.get("l3intf", "")
+                    l3intf_info = f" with peer layer3 interface {peer_l3intf}" if peer_l3intf else ""
+                    remote_disc_info = f" with remote disc {remote_disc}" if remote_disc == 0 else ""
+                    remote_disc_info = f" and remote disc {remote_disc}" if remote_disc == 0 and peer_l3intf else remote_disc_info
+                    last_down = peer_data["lastDown"]
+                    hours_difference = (datetime.fromtimestamp(current_timestamp) - datetime.fromtimestamp(last_down)).total_seconds() / 3600
+                    # Check if peer status is not up
+                    if peer_status != "up":
+                        down_failures.append(f"{peer} is {peer_status} in {vrf} VRF{l3intf_info}{remote_disc_info}.")
+                    # Check if the last down is within the threshold
+                    elif self.inputs.last_down and hours_difference > self.inputs.last_down:
+                        up_failures.append(f"{peer} in {vrf} VRF was down {round(hours_difference)} hours ago{l3intf_info}{remote_disc_info}.")
+                    # Check if remote disc is 0
+                    elif remote_disc == 0:
+                        up_failures.append(f"{peer} in {vrf} VRF has remote disc {remote_disc}{l3intf_info}.")
 
         # Check if there are any failures
         if down_failures:
--- tests/units/anta_tests/test_bfd.py
+++ tests/units/anta_tests/test_bfd.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023 Arista Networks, Inc.
+# Copyright (c) 2023-2024 Arista Networks, Inc.
 # Use of this source code is governed by the Apache License 2.0
 # that can be found in the LICENSE file.
 """
@@ -35,7 +35,22 @@ DATA: list[dict[str, Any]] = [
                             },
                         },
                         "ipv6Neighbors": {},
-                    }
+                    },
+                    "MGMT": {
+                        "ipv4Neighbors": {
+                            "192.0.255.71": {
+                                "peerStats": {
+                                    "": {
+                                        "status": "up",
+                                        "remoteDisc": 3940685114,
+                                        "lastDown": 1703657258.652725,
+                                        "l3intf": "",
+                                    }
+                                }
+                            },
+                        },
+                        "ipv6Neighbors": {},
+                    },
                 }
             },
             {
@@ -49,7 +64,25 @@ DATA: list[dict[str, Any]] = [
         "name": "success-no-peer",
         "test": VerifyBFDPeersHealth,
         "eos_data": [
-            {"vrfs": {}},
+            {
+                "vrfs": {
+                    "MGMT": {
+                        "ipv6Neighbors": {
+                            "2266:25::12:0:ad12": {
+                                "peerStats": {
+                                    "": {
+                                        "status": "up",
+                                        "remoteDisc": 3940685114,
+                                        "lastDown": 1703657258.652725,
+                                        "l3intf": "",
+                                    }
+                                }
+                            },
+                        },
+                        "ipv4Neighbors": {},
+                    }
+                }
+            },
             {
                 "utcTime": 1703658481.8778424,
             },
@@ -75,9 +108,34 @@ DATA: list[dict[str, Any]] = [
                                     }
                                 }
                             },
+                            "192.0.255.70": {
+                                "peerStats": {
+                                    "": {
+                                        "status": "up",
+                                        "remoteDisc": 3940685114,
+                                        "lastDown": 1703657258.652725,
+                                        "l3intf": "",
+                                    }
+                                }
+                            },
                         },
                         "ipv6Neighbors": {},
-                    }
+                    },
+                    "MGMT": {
+                        "ipv4Neighbors": {
+                            "192.0.255.71": {
+                                "peerStats": {
+                                    "": {
+                                        "status": "down",
+                                        "remoteDisc": 0,
+                                        "lastDown": 1703657258.652725,
+                                        "l3intf": "",
+                                    }
+                                }
+                            },
+                        },
+                        "ipv6Neighbors": {},
+                    },
                 }
             },
             {
@@ -87,7 +145,9 @@ DATA: list[dict[str, Any]] = [
         "inputs": {"last_down": 2},
         "expected": {
             "result": "failure",
-            "messages": ["Following BFD peers are not up:\n192.0.255.7 is down in default VRF with remote disc 0."],
+            "messages": [
+                "Following BFD peers are not up:\n192.0.255.7 is down in default VRF with remote disc 0.\n192.0.255.71 is down in MGMT VRF with remote disc 0."
+            ],
         },
     },
     {
@@ -108,9 +168,34 @@ DATA: list[dict[str, Any]] = [
                                     }
                                 }
                             },
+                            "192.0.255.1": {
+                                "peerStats": {
+                                    "": {
+                                        "status": "down",
+                                        "remoteDisc": 0,
+                                        "lastDown": 1703657258.652725,
+                                        "l3intf": "Ethernet3",
+                                    }
+                                }
+                            },
                         },
                         "ipv6Neighbors": {},
-                    }
+                    },
+                    "MGMT": {
+                        "ipv4Neighbors": {
+                            "192.0.255.71": {
+                                "peerStats": {
+                                    "": {
+                                        "status": "down",
+                                        "remoteDisc": 0,
+                                        "lastDown": 1703657258.652725,
+                                        "l3intf": "Ethernet12",
+                                    }
+                                }
+                            },
+                        },
+                        "ipv6Neighbors": {},
+                    },
                 }
             },
             {
@@ -120,7 +205,12 @@ DATA: list[dict[str, Any]] = [
         "inputs": {"last_down": 2},
         "expected": {
             "result": "failure",
-            "messages": ["Following BFD peers are not up:\n192.0.255.7 is down in default VRF with peer layer3 interface Ethernet2 and remote disc 0."],
+            "messages": [
+                "Following BFD peers are not up:\n"
+                "192.0.255.7 is down in default VRF with peer layer3 interface Ethernet2 and remote disc 0.\n"
+                "192.0.255.1 is down in default VRF with peer layer3 interface Ethernet3 and remote disc 0.\n"
+                "192.0.255.71 is down in MGMT VRF with peer layer3 interface Ethernet12 and remote disc 0."
+            ],
         },
     },
     {
@@ -132,6 +222,16 @@ DATA: list[dict[str, Any]] = [
                     "default": {
                         "ipv4Neighbors": {
                             "192.0.255.7": {
+                                "peerStats": {
+                                    "": {
+                                        "status": "up",
+                                        "remoteDisc": 0,
+                                        "lastDown": 1703657258.652725,
+                                        "l3intf": "Ethernet2",
+                                    }
+                                }
+                            },
+                            "192.0.255.71": {
                                 "peerStats": {
                                     "": {
                                         "status": "up",
@@ -153,7 +253,11 @@ DATA: list[dict[str, Any]] = [
         "inputs": {},
         "expected": {
             "result": "failure",
-            "messages": ["Following BFD peers were down:\n192.0.255.7 in default VRF has remote disc 0 with peer layer3 interface Ethernet2."],
+            "messages": [
+                "Following BFD peers were down:\n"
+                "192.0.255.7 in default VRF has remote disc 0 with peer layer3 interface Ethernet2.\n"
+                "192.0.255.71 in default VRF has remote disc 0 with peer layer3 interface Ethernet2."
+            ],
         },
     },
     {
@@ -174,9 +278,44 @@ DATA: list[dict[str, Any]] = [
                                     }
                                 }
                             },
+                            "192.0.255.70": {
+                                "peerStats": {
+                                    "": {
+                                        "status": "up",
+                                        "remoteDisc": 3940685114,
+                                        "lastDown": 1703657258.652725,
+                                        "l3intf": "Ethernet1",
+                                    }
+                                }
+                            },
                         },
                         "ipv6Neighbors": {},
-                    }
+                    },
+                    "MGMT": {
+                        "ipv4Neighbors": {
+                            "192.0.255.17": {
+                                "peerStats": {
+                                    "": {
+                                        "status": "up",
+                                        "remoteDisc": 3940685114,
+                                        "lastDown": 1703657258.652725,
+                                        "l3intf": "Ethernet3",
+                                    }
+                                }
+                            },
+                            "192.0.255.27": {
+                                "peerStats": {
+                                    "": {
+                                        "status": "up",
+                                        "remoteDisc": 3940685114,
+                                        "lastDown": 1703657258.652725,
+                                        "l3intf": "Ethernet4",
+                                    }
+                                }
+                            },
+                        },
+                        "ipv6Neighbors": {},
+                    },
                 }
             },
             {
@@ -186,7 +325,13 @@ DATA: list[dict[str, Any]] = [
         "inputs": {"last_down": 2},
         "expected": {
             "result": "failure",
-            "messages": ["Following BFD peers were down:\n192.0.255.7 in default VRF was down 3 hours ago with peer layer3 interface Ethernet2."],
+            "messages": [
+                "Following BFD peers were down:\n"
+                "192.0.255.7 in default VRF was down 3 hours ago with peer layer3 interface Ethernet2.\n"
+                "192.0.255.70 in default VRF was down 3 hours ago with peer layer3 interface Ethernet1.\n"
+                "192.0.255.17 in MGMT VRF was down 3 hours ago with peer layer3 interface Ethernet3.\n"
+                "192.0.255.27 in MGMT VRF was down 3 hours ago with peer layer3 interface Ethernet4."
+            ],
         },
     },
     {
@@ -198,6 +343,26 @@ DATA: list[dict[str, Any]] = [
                     "default": {
                         "ipv4Neighbors": {
                             "192.0.255.7": {
+                                "peerStats": {
+                                    "": {
+                                        "status": "up",
+                                        "remoteDisc": 3940685114,
+                                        "lastDown": 1703657258.652725,
+                                        "l3intf": "",
+                                    }
+                                }
+                            },
+                            "192.0.255.71": {
+                                "peerStats": {
+                                    "": {
+                                        "status": "up",
+                                        "remoteDisc": 3940685114,
+                                        "lastDown": 1703657258.652725,
+                                        "l3intf": "",
+                                    }
+                                }
+                            },
+                            "192.0.255.17": {
                                 "peerStats": {
                                     "": {
                                         "status": "up",
@@ -219,7 +384,10 @@ DATA: list[dict[str, Any]] = [
         "inputs": {"last_down": 2},
         "expected": {
             "result": "failure",
-            "messages": ["Following BFD peers were down:\n192.0.255.7 in default VRF was down 3 hours ago."],
+            "messages": [
+                "Following BFD peers were down:\n192.0.255.7 in default VRF was down 3 hours ago.\n"
+                "192.0.255.71 in default VRF was down 3 hours ago.\n192.0.255.17 in default VRF was down 3 hours ago."
+            ],
         },
     },
 ]
