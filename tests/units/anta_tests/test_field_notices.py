4b5bc67017efa917af02731c180d3793fc9d53d7
gmuloc <gmulocher@arista.com>
2024-03-25 15:04:07 +0100

    Test: Field notice

--- tests/units/anta_tests/test_field_notices.py
+++ tests/units/anta_tests/test_field_notices.py
@@ -2,7 +2,6 @@
 # Use of this source code is governed by the Apache License 2.0
 # that can be found in the LICENSE file.
 """Test inputs for anta.tests.field_notices."""
-
 from __future__ import annotations
 
 from typing import Any
@@ -21,7 +20,9 @@ DATA: list[dict[str, Any]] = [
                 "modelName": "DCS-7280QRA-C36S",
                 "details": {
                     "deviations": [],
-                    "components": [{"name": "Aboot", "version": "Aboot-veos-8.0.0-3255441"}],
+                    "components": [
+                        {"name": "Aboot", "version": "Aboot-veos-8.0.0-3255441"}
+                    ],
                 },
             },
         ],
@@ -38,12 +39,17 @@ DATA: list[dict[str, Any]] = [
                 "modelName": "DCS-7280QRA-C36S",
                 "details": {
                     "deviations": [],
-                    "components": [{"name": "Aboot", "version": "Aboot-veos-4.0.1-3255441"}],
+                    "components": [
+                        {"name": "Aboot", "version": "Aboot-veos-4.0.1-3255441"}
+                    ],
                 },
             },
         ],
         "inputs": None,
-        "expected": {"result": "failure", "messages": ["device is running incorrect version of aboot (4.0.1)"]},
+        "expected": {
+            "result": "failure",
+            "messages": ["device is running incorrect version of aboot (4.0.1)"],
+        },
     },
     {
         "name": "failure-4.1",
@@ -55,12 +61,17 @@ DATA: list[dict[str, Any]] = [
                 "modelName": "DCS-7280QRA-C36S",
                 "details": {
                     "deviations": [],
-                    "components": [{"name": "Aboot", "version": "Aboot-veos-4.1.0-3255441"}],
+                    "components": [
+                        {"name": "Aboot", "version": "Aboot-veos-4.1.0-3255441"}
+                    ],
                 },
             },
         ],
         "inputs": None,
-        "expected": {"result": "failure", "messages": ["device is running incorrect version of aboot (4.1.0)"]},
+        "expected": {
+            "result": "failure",
+            "messages": ["device is running incorrect version of aboot (4.1.0)"],
+        },
     },
     {
         "name": "failure-6.0",
@@ -72,12 +83,17 @@ DATA: list[dict[str, Any]] = [
                 "modelName": "DCS-7280QRA-C36S",
                 "details": {
                     "deviations": [],
-                    "components": [{"name": "Aboot", "version": "Aboot-veos-6.0.1-3255441"}],
+                    "components": [
+                        {"name": "Aboot", "version": "Aboot-veos-6.0.1-3255441"}
+                    ],
                 },
             },
         ],
         "inputs": None,
-        "expected": {"result": "failure", "messages": ["device is running incorrect version of aboot (6.0.1)"]},
+        "expected": {
+            "result": "failure",
+            "messages": ["device is running incorrect version of aboot (6.0.1)"],
+        },
     },
     {
         "name": "failure-6.1",
@@ -89,12 +105,17 @@ DATA: list[dict[str, Any]] = [
                 "modelName": "DCS-7280QRA-C36S",
                 "details": {
                     "deviations": [],
-                    "components": [{"name": "Aboot", "version": "Aboot-veos-6.1.1-3255441"}],
+                    "components": [
+                        {"name": "Aboot", "version": "Aboot-veos-6.1.1-3255441"}
+                    ],
                 },
             },
         ],
         "inputs": None,
-        "expected": {"result": "failure", "messages": ["device is running incorrect version of aboot (6.1.1)"]},
+        "expected": {
+            "result": "failure",
+            "messages": ["device is running incorrect version of aboot (6.1.1)"],
+        },
     },
     {
         "name": "skipped-model",
@@ -106,12 +127,17 @@ DATA: list[dict[str, Any]] = [
                 "modelName": "vEOS-lab",
                 "details": {
                     "deviations": [],
-                    "components": [{"name": "Aboot", "version": "Aboot-veos-8.0.0-3255441"}],
+                    "components": [
+                        {"name": "Aboot", "version": "Aboot-veos-8.0.0-3255441"}
+                    ],
                 },
             },
         ],
         "inputs": None,
-        "expected": {"result": "skipped", "messages": ["device is not impacted by FN044"]},
+        "expected": {
+            "result": "skipped",
+            "messages": ["device is not impacted by FN044"],
+        },
     },
     {
         "name": "success-JPE",
@@ -207,7 +233,10 @@ DATA: list[dict[str, Any]] = [
             },
         ],
         "inputs": None,
-        "expected": {"result": "skipped", "messages": ["Platform is not impacted by FN072"]},
+        "expected": {
+            "result": "skipped",
+            "messages": ["Platform is not impacted by FN072"],
+        },
     },
     {
         "name": "skipped-range-JPE",
@@ -216,6 +245,38 @@ DATA: list[dict[str, Any]] = [
             {
                 "modelName": "DCS-7280SR3-48YC8",
                 "serialNumber": "JPE2131000",
+                "details": {
+                    "deviations": [],
+                    "components": [{"name": "FixedSystemvrm1", "version": "5"}],
+                },
+            },
+        ],
+        "inputs": None,
+        "expected": {"result": "skipped", "messages": ["Device not exposed"]},
+    },
+    {
+        "name": "skipped-range-K-JPE",
+        "test": VerifyFieldNotice72Resolution,
+        "eos_data": [
+            {
+                "modelName": "DCS-7280SR3K-48YC8",
+                "serialNumber": "JPE2134000",
+                "details": {
+                    "deviations": [],
+                    "components": [{"name": "FixedSystemvrm1", "version": "5"}],
+                },
+            },
+        ],
+        "inputs": None,
+        "expected": {"result": "skipped", "messages": ["Device not exposed"]},
+    },
+    {
+        "name": "skipped-range-JAS",
+        "test": VerifyFieldNotice72Resolution,
+        "eos_data": [
+            {
+                "modelName": "DCS-7280SR3-48YC8",
+                "serialNumber": "JAS2041000",
                 "details": {
                     "deviations": [],
                     "components": [{"name": "FixedSystemvrm1", "version": "5"}],
@@ -287,6 +348,9 @@ DATA: list[dict[str, Any]] = [
             },
         ],
         "inputs": None,
-        "expected": {"result": "error", "messages": ["Error in running test - FixedSystemvrm1 not found"]},
+        "expected": {
+            "result": "error",
+            "messages": ["Error in running test - FixedSystemvrm1 not found"],
+        },
     },
 ]
