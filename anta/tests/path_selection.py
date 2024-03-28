2d0df4088b6de26032298b3f8b42b1046f4d7f38
Mahesh Kumar <mahesh.kumar-ext@arista.com>
2024-03-28 06:29:11 -0700

    issue-577: Fixed the ruff linting

--- anta/tests/path_selection.py
+++ anta/tests/path_selection.py
@@ -1,17 +1,14 @@
 # Copyright (c) 2023-2024 Arista Networks, Inc.
 # Use of this source code is governed by the Apache License 2.0
 # that can be found in the LICENSE file.
-"""
-Test functions related to various router path-selection settings
-"""
+"""Test functions related to various router path-selection settings."""
+
 # Mypy does not understand AntaTest.Input typing
 # mypy: disable-error-code=attr-defined
 from __future__ import annotations
 
 from ipaddress import IPv4Address
-
-# Need to keep List for pydantic in python 3.8
-from typing import List
+from typing import ClassVar
 
 from pydantic import BaseModel
 
@@ -23,18 +20,27 @@ class VerifyRouterPathsHealth(AntaTest):
     """
     Verifies the state of all paths under router path-selection as ipsecEstablished.
 
-    Expected Results:
-        * success: The test will pass if all the paths under router path-selection are ipsecEstablished.
-        * failure: The test will fail if router path-selection is not configured or any path's state is not ipsecEstablished.
+    Expected Results
+    ----------------
+    * Success: The test will pass if all the paths under router path-selection are ipsecEstablished.
+    * Failure: The test will fail if router path-selection is not configured or any path's state is not ipsecEstablished.
+
+    Examples
+    --------
+    ```yaml
+    anta.tests.path_selection:
+      - VerifyRouterPathsHealth:
+    ```
     """
 
     name = "VerifyRouterPathsHealth"
     description = "Verifies the state of all paths under router path-selection as ipsecEstablished."
-    categories = ["path-selection"]
-    commands = [AntaCommand(command="show path-selection paths")]
+    categories: ClassVar[list[str]] = ["path-selection"]
+    commands: ClassVar[list[AntaCommand | AntaTemplate]] = [AntaCommand(command="show path-selection paths")]
 
     @AntaTest.anta_test
     def test(self) -> None:
+        """Main test function for VerifyRouterPathsHealth."""
         self.result.is_success()
 
         command_output = self.instance_commands[0].json_output["dpsPeers"]
@@ -48,7 +54,7 @@ class VerifyRouterPathsHealth(AntaTest):
         failed_log = ""
         for peer, peer_data in command_output.items():
             for group, group_data in peer_data["dpsGroups"].items():
-                for _, path_data in group_data["dpsPaths"].items():
+                for path_data in group_data["dpsPaths"].values():
                     state = path_data["state"]
 
                     # If the state of any path is not 'ipsecEstablished', the test fails
@@ -60,38 +66,53 @@ class VerifyRouterPathsHealth(AntaTest):
 
 class VerifySpecificRouterPath(AntaTest):
     """
-    Verifies the state of a specific path under router path-selection as ipsecEstablished.
+    Verifies the state of a specific path under router path-selection for an IPv4 peer as ipsecEstablished.
 
-    Expected Results:
-        * success: The test will pass if the input path under router path-selection is ipsecEstablished.
-        * failure: The test will fail if the input path is not found or its state is not ipsecEstablished.
+    Expected Results
+    ----------------
+    * Success: The test will pass if the input path under router path-selection is ipsecEstablished.
+    * Failure: The test will fail if the input path is not found or its state is not ipsecEstablished.
+
+    Examples
+    --------
+    ```yaml
+    anta.tests.path_selection:
+      - VerifySpecificRouterPath:
+          paths:
+            - peer: 10.255.0.1
+              path_groups:
+                - internet
+                - mpls
+    ```
     """
 
     name = "VerifySpecificRouterPath"
-    description = "Verifies the state of a specific path under router path-selection as ipsecEstablished."
-    categories = ["path-selection"]
-    commands = [AntaTemplate(template="show path-selection paths peer {peer} path-group {group}")]
+    description = "Verifies the state of a specific path under router path-selection for an IPv4 peer as ipsecEstablished."
+    categories: ClassVar[list[str]] = ["path-selection"]
+    commands: ClassVar[list[AntaCommand | AntaTemplate]] = [AntaTemplate(template="show path-selection paths peer {peer} path-group {group}")]
 
     class Input(AntaTest.Input):
-        """Inputs for the VerifySpecificRouterPath test."""
+        """Input model for the VerifySpecificRouterPath test."""
 
-        paths: List[RouterPath]
-        """List of router paths to verify"""
+        paths: list[RouterPath]
+        """List of router paths to verify."""
 
         class RouterPath(BaseModel):
-            """Detail of a router path"""
+            """Detail of a router path."""
 
             peer: IPv4Address
-            """Static peer IPv4 address"""
+            """Static peer IPv4 address."""
 
-            path_groups: List[str]
-            """Router path group names"""
+            path_groups: list[str]
+            """Router path group names."""
 
     def render(self, template: AntaTemplate) -> list[AntaCommand]:
+        """Render the template for each router path."""
         return [template.render(peer=path.peer, group=path_group) for path in self.inputs.paths for path_group in path.path_groups]
 
     @AntaTest.anta_test
     def test(self) -> None:
+        """Main test function for VerifySpecificRouterPath."""
         self.result.is_success()
 
         # Check the state of each path
@@ -107,7 +128,7 @@ class VerifySpecificRouterPath(AntaTest):
 
             # Extract the state of the path
             path_output = get_value(command_output, f"{peer}..dpsGroups..{path_group}..dpsPaths", separator="..")
-            state = list(path_output.values())[0].get("state")
+            state = next(iter(path_output.values())).get("state")
 
             # If the state of the path is not 'ipsecEstablished', the test fails
             if state != "ipsecEstablished":
