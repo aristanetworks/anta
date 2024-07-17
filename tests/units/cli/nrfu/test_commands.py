3baf7ae1217a5f8850bee1422d3e8eb6eee65ade
gmuloc <gmulocher@arista.com>
2024-07-17 15:40:31 +0200

    Test: Avoid creating test.csv in repo

--- tests/units/cli/nrfu/test_commands.py
+++ tests/units/cli/nrfu/test_commands.py
@@ -97,16 +97,20 @@ def test_anta_nrfu_template(click_runner: CliRunner) -> None:
     assert "* VerifyEOSVersion is SUCCESS for dummy" in result.output
 
 
-def test_anta_nrfu_csv(click_runner: CliRunner) -> None:
+def test_anta_nrfu_csv(click_runner: CliRunner, tmp_path: Path) -> None:
     """Test anta nrfu csv."""
-    result = click_runner.invoke(anta, ["nrfu", "csv", "--csv-output", "test.csv"])
+    csv_output = tmp_path / "test.csv"
+    result = click_runner.invoke(anta, ["nrfu", "csv", "--csv-output", str(csv_output)])
     assert result.exit_code == ExitCode.OK
     assert "CSV report saved to" in result.output
+    assert csv_output.exists()
 
 
-def test_anta_nrfu_csv_failure(click_runner: CliRunner) -> None:
+def test_anta_nrfu_csv_failure(click_runner: CliRunner, tmp_path: Path) -> None:
     """Test anta nrfu csv."""
+    csv_output = tmp_path / "test.csv"
     with patch("anta.reporter.csv_reporter.ReportCsv.generate", side_effect=OSError()):
-        result = click_runner.invoke(anta, ["nrfu", "csv", "--csv-output", "read_test.csv"])
+        result = click_runner.invoke(anta, ["nrfu", "csv", "--csv-output", str(csv_output)])
     assert result.exit_code == ExitCode.USAGE_ERROR
     assert "Failed to save CSV report to" in result.output
+    assert not csv_output.exists()
