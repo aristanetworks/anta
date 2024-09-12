6b5113201ff3a73dc1e090324cf5d81a94d57162
Matthieu Tâche <tache.matthieu@gmail.com>
2024-09-12 17:30:44 +0200

    refactor: addressing comments

--- tests/units/result_manager/test_models.py
+++ tests/units/result_manager/test_models.py
@@ -15,14 +15,15 @@ from tests.lib.fixture import DEVICE_NAME
 if TYPE_CHECKING:
     from _pytest.mark.structures import ParameterSet
 
+    # Import as Result to avoid pytest collection
     from anta.result_manager.models import TestResult as Result
 
 TEST_RESULT_SET_STATUS: list[ParameterSet] = [
-    pytest.param(AntaTestStatus.SUCCESS, "success", id="set_success"),
-    pytest.param(AntaTestStatus.ERROR, "error", id="set_error"),
-    pytest.param(AntaTestStatus.FAILURE, "failure", id="set_failure"),
-    pytest.param(AntaTestStatus.SKIPPED, "skipped", id="set_skipped"),
-    pytest.param(AntaTestStatus.UNSET, "unset", id="set_unset"),
+    pytest.param(AntaTestStatus.SUCCESS, "test success message", id="set_success"),
+    pytest.param(AntaTestStatus.ERROR, "test error message", id="set_error"),
+    pytest.param(AntaTestStatus.FAILURE, "test failure message", id="set_failure"),
+    pytest.param(AntaTestStatus.SKIPPED, "test skipped message", id="set_skipped"),
+    pytest.param(AntaTestStatus.UNSET, "test unset message", id="set_unset"),
 ]
 
 
@@ -37,11 +38,11 @@ class TestTestResultModels:
         assert len(testresult.messages) == 0
         if target == AntaTestStatus.SUCCESS:
             testresult.is_success(message)
-            assert testresult.result == target
+            assert testresult.result == "success"
             assert message in testresult.messages
         if target == AntaTestStatus.FAILURE:
             testresult.is_failure(message)
-            assert testresult.result == target
+            assert testresult.result == "failure"
             assert message in testresult.messages
         if target == AntaTestStatus.ERROR:
             testresult.is_error(message)
@@ -49,12 +50,12 @@ class TestTestResultModels:
             assert message in testresult.messages
         if target == AntaTestStatus.SKIPPED:
             testresult.is_skipped(message)
-            assert testresult.result == target
+            assert testresult.result == "skipped"
             assert message in testresult.messages
-        # no helper for unset, testing _set_status
         if target == AntaTestStatus.UNSET:
+            # no helper for unset, testing _set_status
             testresult._set_status(AntaTestStatus.UNSET, message)  # pylint: disable=W0212
-            assert testresult.result == target
+            assert testresult.result == "unset"
             assert message in testresult.messages
 
     @pytest.mark.parametrize(("target", "message"), TEST_RESULT_SET_STATUS)
