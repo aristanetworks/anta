0cc37098c7d6e6cdd23c310792e9e9b4f4d35e98
Geetanjali G Mane <geetanjali.mane@GSLAB.COM>
2024-11-21 17:24:52 +0530

    Issue_884: fixed trailling white spaces

--- tests/units/anta_tests/routing/test_generic.py
+++ tests/units/anta_tests/routing/test_generic.py
@@ -459,10 +459,7 @@ DATA: list[dict[str, Any]] = [
         },
         "expected": {
             "result": "failure",
-            "messages": [
-                "For following routes, VRF is not configured or Route types are invalid:\n{'routes_entries': {"
-                "'10.100.0.12/32': {'default': 'Routes not found.'}}}"
-            ],
+            "messages": ["Prefix: 10.100.0.12/32 VRF: default - Routes not found"],
         },
     },
     {
@@ -545,10 +542,9 @@ DATA: list[dict[str, Any]] = [
         "expected": {
             "result": "failure",
             "messages": [
-                "For following routes, VRF is not configured or Route types are invalid:\n"
-                "{'routes_entries': {'10.10.0.1/32': {'default': {'route_type': \"Expected route type is 'iBGP' however in actual it is found as 'eBGP'\"}}, "
-                "'10.100.0.14/31': {'default': {'route_type': \"Expected route type is 'static' however in actual it is found as 'connected'\"}}, "
-                "'10.100.1.5/32': {'default': {'route_type': \"Expected route type is 'eBGP' however in actual it is found as 'iBGP'\"}}}}"
+                "Prefix: 10.10.0.1/32 VRF: default -  Expected route type is 'iBGP' however in actual it is found as 'eBGP'",
+                "Prefix: 10.100.0.14/31 VRF: default -  Expected route type is 'static' however in actual it is found as 'connected'",
+                "Prefix: 10.100.1.5/32 VRF: default -  Expected route type is 'eBGP' however in actual it is found as 'iBGP'",
             ],
         },
     },
@@ -568,12 +564,11 @@ DATA: list[dict[str, Any]] = [
         "expected": {
             "result": "failure",
             "messages": [
-                "For following routes, VRF is not configured or Route types are invalid:\n"
-                "{'routes_entries': {'10.10.0.1/32': {'default': 'Not configured'}, "
-                "'10.100.0.12/31': {'default': 'Not configured'}, "
-                "'10.100.0.14/31': {'default': 'Not configured'}, "
-                "'10.100.0.128/31': {'default': 'Not configured'}, "
-                "'10.100.1.5/32': {'default': 'Not configured'}}}"
+                "Prefix 10.10.0.1/32 - VRF default is not configured",
+                "Prefix 10.100.0.12/31 - VRF default is not configured",
+                "Prefix 10.100.0.14/31 - VRF default is not configured",
+                "Prefix 10.100.0.128/31 - VRF default is not configured",
+                "Prefix 10.100.1.5/32 - VRF default is not configured",
             ],
         },
     },
