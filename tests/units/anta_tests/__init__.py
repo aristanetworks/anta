105ad00f03dcfacab1f78aa0e8020dab7637b47d
Matthieu Tâche <tache.matthieu@gmail.com>
2024-11-26 19:11:56 +0100

    fix: syntax error py<311

--- tests/units/anta_tests/__init__.py
+++ tests/units/anta_tests/__init__.py
@@ -26,7 +26,7 @@ def test(device: AntaDevice, data: dict[str, Any]) -> None:
         # We expect messages in test result
         assert len(test_instance.result.messages) == len(
             data["expected"]["messages"]
-        ), f"Expected {len(data["expected"]["messages"])} messages, got {len(test_instance.result.messages)}"
+        ), f"Expected {len(data['expected']['messages'])} messages, got {len(test_instance.result.messages)}"
         # Test will pass if the expected message is included in the test result message
         for message, expected in zip(test_instance.result.messages, data["expected"]["messages"]):  # NOTE: zip(strict=True) has been added in Python 3.10
             assert expected in message
@@ -37,7 +37,7 @@ def test(device: AntaDevice, data: dict[str, Any]) -> None:
     if "atomic_results" in data["expected"]:
         assert len(test_instance.result.atomic_results) == len(
             data["expected"]["atomic_results"]
-        ), f"Expected {len(data["expected"]["atomic_results"])} atomic results, got {len(test_instance.result.atomic_results)}"
+        ), f"Expected {len(data['expected']['atomic_results'])} atomic results, got {len(test_instance.result.atomic_results)}"
         for result, expected in zip(test_instance.result.atomic_results, data["expected"]["atomic_results"]):
             assert result.model_dump(serialize_as_any=True, mode="json") == expected
     else:
