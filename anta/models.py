e6e1474ff6414762079730fb000558a46cefb6c7
gmuloc <gmulocher@arista.com>
2024-12-11 16:34:17 +0100

    fix: Bad stuff @gmuloc

--- anta/models.py
+++ anta/models.py
@@ -270,7 +270,8 @@ class AntaCommand(BaseModel):
         if not self.collected and not self.error:
             msg = f"Command '{self.command}' has not been collected and has not returned an error. Call AntaDevice.collect()."
             raise RuntimeError(msg)
-        return not any("not supported on this hardware platform" in e for e in self.errors)
+        known_eos_errors = ["BGP inactive", "VRF '.*' is not active", ".* does not support IP "]
+        return any(any(re.match(pattern, e) for e in self.errors) for pattern in known_eos_errors)
 
 
 class AntaTemplateRenderError(RuntimeError):
