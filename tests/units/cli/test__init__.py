e13156fe3115b6806ca0b8d9955459bc3e2f9c7a
gmuloc <gmulocher@arista.com>
2025-04-07 16:09:36 +0200

    refactor: Do not over eager catch all import errors as missing anta[cli]

--- tests/units/cli/test__init__.py
+++ tests/units/cli/test__init__.py
@@ -6,13 +6,10 @@
 from __future__ import annotations
 
 import sys
-from importlib import reload
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING, Any, Callable
 from unittest.mock import patch
 
 import pytest
-
-import anta.cli
 
 if TYPE_CHECKING:
     from types import ModuleType
@@ -20,21 +17,28 @@ if TYPE_CHECKING:
 builtins_import = __import__
 
 
-# Tried to achieve this with mock
 # http://materials-scientist.com/blog/2021/02/11/mocking-failing-module-import-python/
-def import_mock(name: str, *args: Any) -> ModuleType:  # noqa: ANN401
-    """Mock."""
-    if name == "click":
-        msg = "No module named 'click'"
-        raise ModuleNotFoundError(msg)
-    return builtins_import(name, *args)
+def import_mock(delete_name: str) -> Callable[..., ModuleType]:
+    """Mock missing the package 'delete_name' when importing."""
+
+    def wrapper(name: str, *args: Any) -> ModuleType:  # noqa: ANN401
+        """Mock."""
+        if name == delete_name:
+            msg = f"No module named '{delete_name}'"
+            raise ModuleNotFoundError(msg, name=delete_name)
+        return builtins_import(name, *args)
+
+    return wrapper
 
 
-def test_cli_error_missing(capsys: pytest.CaptureFixture[Any]) -> None:
+def test_cli_error_missing_click(capsys: pytest.CaptureFixture[Any]) -> None:
+    # def test_cli_error_missing_click() -> None:
     """Test ANTA errors out when anta[cli] was not installed."""
-    with patch.dict(sys.modules) as sys_modules, patch("builtins.__import__", import_mock):
-        del sys_modules["anta.cli._main"]
-        reload(anta.cli)
+    with patch.dict(sys.modules) as sys_modules, patch("builtins.__import__", import_mock("click")):
+        for k in list(sys_modules.keys()):
+            if k.startswith("anta."):  # and k != "anta.cli":
+                del sys_modules[k]
+        import anta.cli
 
         with pytest.raises(SystemExit) as e_info:
             anta.cli.cli()
@@ -53,3 +57,16 @@ def test_cli_error_missing(capsys: pytest.CaptureFixture[Any]) -> None:
         assert "Make sure you've installed everything with: pip install 'anta[cli]'" in captured.out
         assert "The caught exception was:" in captured.out
         assert e_info.value.code == 1
+
+
+def test_cli_error_missing_other() -> None:
+    """Test ANTA errors out when anta[cli] was not installed."""
+    with patch.dict(sys.modules) as sys_modules, patch("builtins.__import__", import_mock("httpx")):
+        # Need to clean up from previous runs a path that will trigger reimporting httpx
+        for k in list(sys_modules.keys()):
+            if k.startswith("anta."):  # and k != "anta.cli":
+                del sys_modules[k]
+        import anta.cli
+
+        with pytest.raises(ImportError, match="httpx"):
+            anta.cli.cli()
