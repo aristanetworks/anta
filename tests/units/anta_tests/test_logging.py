41929d02c0d9d270d899a6c39d665654bb31b28b
vitthalmagadum <vitthal.magadum-ext@arista.com>
2025-04-08 08:19:25 -0400

    Updated test VerifyLoggingErrors to add logs in failure msg

--- tests/units/anta_tests/test_logging.py
+++ tests/units/anta_tests/test_logging.py
@@ -277,7 +277,14 @@ DATA: list[dict[str, Any]] = [
             "Aug  2 19:57:42 DC1-LEAF1A Mlag: %FWK-3-SOCKET_CLOSE_REMOTE: Connection to Mlag (pid:27200) at tbt://192.168.0.1:4432/+n closed by peer (EOF)",
         ],
         "inputs": None,
-        "expected": {"result": "failure", "messages": ["Device has reported syslog messages with a severity of ERRORS or higher"]},
+        "expected": {
+            "result": "failure",
+            "messages": [
+                "Device has reported syslog messages at threshold level or above:\n"
+                "Aug  2 19:57:42 DC1-LEAF1A Mlag: %FWK-3-SOCKET_CLOSE_REMOTE: Connection to Mlag "
+                "(pid:27200) at tbt://192.168.0.1:4432/+n closed by peer (EOF)"
+            ]
+        },
     },
     {
         "name": "success",
