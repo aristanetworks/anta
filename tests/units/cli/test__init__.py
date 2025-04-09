3bda35f938580f4f5211a5661a5901eded3a1d97
gmuloc <gmulocher@arista.com>
2025-04-09 11:19:03 +0200

    Test: Ignore some tests in python <3.11 because patch was sad

--- tests/units/cli/test__init__.py
+++ tests/units/cli/test__init__.py
@@ -6,35 +6,17 @@
 from __future__ import annotations
 
 import sys
-from typing import TYPE_CHECKING, Any, Callable
+from typing import Any
 from unittest.mock import patch
 
 import pytest
 
-if TYPE_CHECKING:
-    from types import ModuleType
 
-builtins_import = __import__
-
-
-# http://materials-scientist.com/blog/2021/02/11/mocking-failing-module-import-python/
-def import_mock(delete_name: str) -> Callable[..., ModuleType]:
-    """Mock missing the package 'delete_name' when importing."""
-
-    def wrapper(name: str, *args: Any) -> ModuleType:  # noqa: ANN401
-        """Mock."""
-        if name == delete_name:
-            msg = f"No module named '{delete_name}'"
-            raise ModuleNotFoundError(msg, name=delete_name)
-        return builtins_import(name, *args)
-
-    return wrapper
-
-
+# https://github.com/python/cpython/issues/88852
+@pytest.mark.skipif(sys.version_info <= (3, 11), reason="Unreliable behavior patching sys.modules before 3.11")
 def test_cli_error_missing_click(capsys: pytest.CaptureFixture[Any]) -> None:
-    # def test_cli_error_missing_click() -> None:
     """Test ANTA errors out when anta[cli] was not installed."""
-    with patch.dict(sys.modules) as sys_modules, patch("builtins.__import__", import_mock("click")):
+    with patch.dict(sys.modules, {"click": None}) as sys_modules:
         for k in list(sys_modules.keys()):
             if k.startswith("anta."):  # and k != "anta.cli":
                 del sys_modules[k]
@@ -59,9 +41,11 @@ def test_cli_error_missing_click(capsys: pytest.CaptureFixture[Any]) -> None:
         assert e_info.value.code == 1
 
 
+# https://github.com/python/cpython/issues/88852
+@pytest.mark.skipif(sys.version_info <= (3, 11), reason="Unreliable behavior patching sys.modules before 3.11")
 def test_cli_error_missing_other() -> None:
     """Test ANTA errors out when anta[cli] was not installed."""
-    with patch.dict(sys.modules) as sys_modules, patch("builtins.__import__", import_mock("httpx")):
+    with patch.dict(sys.modules, {"httpx": None}) as sys_modules:
         # Need to clean up from previous runs a path that will trigger reimporting httpx
         for k in list(sys_modules.keys()):
             if k.startswith("anta."):  # and k != "anta.cli":
