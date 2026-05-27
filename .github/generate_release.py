9021f1fdab7d12d81d346c0b8ec21815c3f677ad
gmuloc <gmulocher@arista.com>
2026-05-27 16:38:10 +0200

    CI: Make pre-commit happy again

--- .github/generate_release.py
+++ .github/generate_release.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+# Copyright (c) 2026 Arista Networks, Inc.
+# Use of this source code is governed by the Apache License 2.0
+# that can be found in the LICENSE file.
 """generate_release.py.
 
 This script is used to generate the release.yml file as per
